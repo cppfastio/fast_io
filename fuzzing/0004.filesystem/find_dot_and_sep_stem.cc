2df1fecb126cba6440afdf9f42d4ca90e2d4f13e
expnkx <unlvsur@live.com>
2022-02-07 02:23:06 -0500

    just hack things around to make compilation works for fuzzing

--- fuzzing/0004.filesystem/find_dot_and_sep_stem.cc
+++ fuzzing/0004.filesystem/find_dot_and_sep_stem.cc
@@ -3,7 +3,7 @@
 
 thread_local fast_io::obuf_file obf(u8"/dev/null");
 
-extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* ptr,std::size_t n) noexcept
+extern "C" int LLVMFuzzerTestOneInput(char8_t const* ptr,std::size_t n) noexcept
 {
 	auto e{::fast_io::details::find_dot_and_sep<true,char8_t,char8_t>(ptr,n)};
 	write(obf,e.ptr,e.ptr+e.n);
--- fuzzing/0004.filesystem/find_dot_and_sep_stem2.cc
+++ fuzzing/0004.filesystem/find_dot_and_sep_stem2.cc
@@ -3,9 +3,9 @@
 
 thread_local fast_io::obuf_file obf(u8"/dev/null");
 
-extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* ptr,std::size_t n) noexcept
+extern "C" int LLVMFuzzerTestOneInput(char16_t const* ptr,std::size_t n) noexcept
 {
-	auto e{::fast_io::details::find_dot_and_sep<true,char16_t,char16_t>(ptr,n)};
+	auto e{::fast_io::details::find_dot_and_sep<true,char16_t,char16_t>(ptr,n/2)};
 	write(obf,e.ptr,e.ptr+e.n);
         return 0;
 }
