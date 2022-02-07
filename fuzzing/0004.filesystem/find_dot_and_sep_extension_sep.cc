c21304dfce7044fd3b14d1ca251e0c6412c22064
expnkx <unlvsur@live.com>
2022-02-07 02:17:44 -0500

    Add all fuzzing for filesystem internal apis implementations

---
+++ fuzzing/0004.filesystem/find_dot_and_sep_extension_sep.cc
@@ -0,0 +1,10 @@
+#include<fast_io.h>
+
+thread_local fast_io::obuf_file obf(u8"/dev/null");
+
+extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* ptr,std::size_t n) noexcept
+{
+	auto e{::fast_io::details::find_dot_and_sep<false,char8_t,char>(ptr,n)};
+	write(obf,e.ptr,e.ptr+e.n);
+        return 0;
+}
