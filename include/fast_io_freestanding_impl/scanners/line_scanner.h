817c446f2dd86073e695fce03d5e0547980cc249
expnkx <unlvsur@live.com>
2022-02-17 19:10:32 -0500

    add assert for line_scanner.h

--- include/fast_io_freestanding_impl/scanners/line_scanner.h
+++ include/fast_io_freestanding_impl/scanners/line_scanner.h
@@ -178,6 +178,7 @@ template<std::integral char_type>
 inline constexpr parse_result<char_type const*> scan_iterative_contiguous_line_define_impl(
 	basic_line_scanner_contiguous_view<char_type>& __restrict buf,char_type const* first,char_type const* last) noexcept
 {
+	assert(first<=last);
 	auto it{::fast_io::find_lf(first,last)};
 	if(it==last)[[unlikely]]
 	{
