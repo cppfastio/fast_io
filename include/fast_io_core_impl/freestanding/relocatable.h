8939fbb40cc8163d6c1dc3c9bcb039f1f737bb93
trcrsired <uwgghhbcad@gmail.com>
2022-07-24 21:00:35 -0400

    trivially_relocatable is ridiculous. Cannot detect from clang but clang defines it

--- include/fast_io_core_impl/freestanding/relocatable.h
+++ include/fast_io_core_impl/freestanding/relocatable.h
@@ -6,7 +6,7 @@ namespace fast_io::freestanding
 template<typename T>
 inline constexpr bool is_trivially_relocatable_v =
 #if defined(__has_extension)
-#if __has_extension(is_trivially_relocatable)
+#if __has_extension(trivially_relocatable)
 __is_trivially_relocatable(T) ||
 #endif
 #endif
