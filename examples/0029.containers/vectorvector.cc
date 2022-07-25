4ddd4e93e34fa080bb66542a1c960f1c4778b0dc
trcrsired <uwgghhbcad@gmail.com>
2022-07-24 21:36:49 -0400

    add 2d vector example

---
+++ examples/0029.containers/vectorvector.cc
@@ -0,0 +1,30 @@
+#if defined(USE_STD)
+#include<vector>
+#include<cstdint>
+namespace test
+{
+template<typename T>
+using vector = ::std::vector<T>;
+}
+#else
+#include<fast_io_dsal/vector.h>
+
+namespace test
+{
+template<typename T>
+using vector = ::fast_io::vector<T>;
+}
+#endif
+
+/*
+Unfortunately fast_io internally sometimes uses vector despite i try to avoid them. Well then better expose the APIs
+*/
+
+int main()
+{
+	test::vector<test::vector<std::int_least32_t>> vec1;
+	test::vector<test::vector<char32_t>> vec2;
+	vec1.emplace_back();
+	vec1.emplace_back();
+	vec1.emplace_back();
+}
