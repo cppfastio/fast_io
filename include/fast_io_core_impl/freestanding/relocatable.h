20c35ab7ac7d76789e8d8fe09361766dfa6f87bc
trcrsired <uwgghhbcad@gmail.com>
2022-07-25 15:48:45 -0400

    add sized_based deallocation

--- include/fast_io_core_impl/freestanding/relocatable.h
+++ include/fast_io_core_impl/freestanding/relocatable.h
@@ -15,4 +15,14 @@ struct is_trivially_relocatable
 
 template<typename T>
 inline constexpr bool is_trivially_relocatable_v = is_trivially_relocatable<T>::value;
+
+template<typename T>
+struct is_zero_default_constructible
+{
+	inline static constexpr bool value = ::std::is_scalar_v<T>||::std::is_empty_v<T>;
+};
+
+template<typename T>
+inline constexpr bool is_zero_default_constructible_v = is_zero_default_constructible<T>::value;
+
 }
