d226f2af9691b3a019ac1e478110acf87366dc9f
trcrsired <uwgghhbcad@gmail.com>
2022-08-14 23:31:12 -0400

    Fix around issues with mimalloc

--- benchmark/0011.containers/vector/0002.multi_push_back/std.cc
+++ benchmark/0011.containers/vector/0002.multi_push_back/std.cc
@@ -5,5 +5,5 @@ namespace test
 template<typename T>
 using vector = ::std::vector<T>;
 }
-#define BENCH_VECTOR_COMMENT_STRING u8"std::vector"
+#define BENCH_VECTOR_COMMENT_STRING u8"std::vector<T>"
 #include"main.h"
