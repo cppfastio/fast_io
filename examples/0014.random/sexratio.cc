1b26073a18275089258a0886615da228771f3bb2
trcrsired <uwgghhbcad@gmail.com>
2022-09-15 16:56:43 -0400

    add N

--- examples/0014.random/sexratio.cc
+++ examples/0014.random/sexratio.cc
@@ -2,9 +2,18 @@
 #include<fast_io.h>
 #include<fast_io_device.h>
 
-int main()
+int main(int argc,char** argv)
 {
 	using namespace fast_io::mnp;
+	if(argc<2)
+	{
+		if(argc==0)
+		{
+			return 1;
+		}
+		perr("Usage: ",os_c_str(*argv)," N\n");
+		return 1;
+	}
 	constexpr auto natural_male_to_female_ratio{1.05};
 	constexpr auto total_ratio{natural_male_to_female_ratio+1.0};
 	constexpr auto male_probability{natural_male_to_female_ratio/total_ratio};
@@ -15,7 +24,7 @@ int main()
 	fast_io::u8obuf_file obf(u8"sexratio.txt");
 	print(obf,u8"Natural Male to female birth ratio:",natural_male_to_female_ratio,u8"\n"
 		u8"Male Baby birth Probability:",male_probability,u8"\n\n");	
-	constexpr ::std::uint_least64_t const n{100000};
+	::std::uint_least64_t const n{::fast_io::to<::std::uint_least64_t>(os_c_str(argv[1]))};
 	for(::std::uint_least64_t i{};i!=n;++i)
 	{
 		bool const is_boy{dis(engine)};
