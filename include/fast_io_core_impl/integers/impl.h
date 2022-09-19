07f644506cda698a187c91e242194da972551f43
trcrsired <uwgghhbcad@gmail.com>
2022-09-19 03:16:05 -0400

    add hex0x and hex0xupper manipulator

--- include/fast_io_core_impl/integers/impl.h
+++ include/fast_io_core_impl/integers/impl.h
@@ -203,6 +203,33 @@ inline constexpr scalar_manip_t<::fast_io::details::base_mani_flags_cache<16,tru
 		return {::fast_io::bit_cast<std::uintptr_t>(::fast_io::freestanding::to_address(t))};
 }
 
+template<bool full=false,typename scalar_type>
+requires (::fast_io::details::my_integral<scalar_type>||std::is_pointer_v<std::remove_cvref_t<scalar_type>>||std::same_as<std::nullptr_t,std::remove_cvref_t<scalar_type>>||::fast_io::freestanding::contiguous_iterator<scalar_type>)
+inline constexpr scalar_manip_t<::fast_io::details::base_mani_flags_cache<16,false,true,full>,std::conditional_t<(::fast_io::details::my_integral<scalar_type>),std::remove_cvref_t<scalar_type>,std::uintptr_t>> hex0x(scalar_type t) noexcept
+{
+	if constexpr(std::same_as<scalar_type,std::nullptr_t>)
+		return {};
+	else if constexpr(::fast_io::details::my_integral<scalar_type>)
+		return {t};
+	else if constexpr(std::is_pointer_v<std::remove_cvref_t<scalar_type>>)
+		return {::fast_io::bit_cast<std::uintptr_t>(t)};
+	else
+		return {::fast_io::bit_cast<std::uintptr_t>(::fast_io::freestanding::to_address(t))};
+}
+
+template<bool full=false,typename scalar_type>
+requires (::fast_io::details::my_integral<scalar_type>||std::is_pointer_v<std::remove_cvref_t<scalar_type>>||std::same_as<std::nullptr_t,std::remove_cvref_t<scalar_type>>||::fast_io::freestanding::contiguous_iterator<scalar_type>)
+inline constexpr scalar_manip_t<::fast_io::details::base_mani_flags_cache<16,true,true,full>,std::conditional_t<(::fast_io::details::my_integral<scalar_type>),std::remove_cvref_t<scalar_type>,std::uintptr_t>> hex0xupper(scalar_type t) noexcept
+{
+	if constexpr(std::same_as<scalar_type,std::nullptr_t>)
+		return {};
+	else if constexpr(::fast_io::details::my_integral<scalar_type>)
+		return {t};
+	else if constexpr(std::is_pointer_v<std::remove_cvref_t<scalar_type>>)
+		return {::fast_io::bit_cast<std::uintptr_t>(t)};
+	else
+		return {::fast_io::bit_cast<std::uintptr_t>(::fast_io::freestanding::to_address(t))};
+}
 
 template<bool shbase=false,typename scalar_type>
 requires (::fast_io::details::my_integral<scalar_type>||std::is_pointer_v<std::remove_cvref_t<scalar_type>>||std::same_as<std::nullptr_t,std::remove_cvref_t<scalar_type>>||::fast_io::freestanding::contiguous_iterator<scalar_type>)
