cee491dcfd4d2ba98f94c64d4544b7037ee22f3e
trcrsired <uwgghhbcad@gmail.com>
2022-09-20 21:33:56 -0400

    add an example for randomize pin

---
+++ examples/0014.random/pin.cc
@@ -0,0 +1,25 @@
+﻿#include<fast_io.h>
+#include<fast_io_device.h>
+#include<random>
+
+int main(int argc,char** argv)
+{
+	std::size_t n{100};
+	if(1<argc)
+	{
+		n=fast_io::to<std::size_t>(argv[1]);
+	}
+	fast_io::u8obuf_file obf(u"pin.txt");
+	fast_io::ibuf_white_hole_engine eng;
+	std::uniform_int_distribution<unsigned> ud(0,9);
+	for(std::size_t i{};i!=n;++i)
+	{
+		for(std::size_t j{};j!=4zu;++j)
+		{
+			char8_t ch(static_cast<char8_t>(ud(eng)));
+			ch+=u8'0';
+			print(obf,fast_io::mnp::chvw(ch));
+		}
+		println(obf);
+	}
+}
