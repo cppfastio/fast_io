187c117f2e1699bac062de28bccf2199baa6010e
trcrsired <uwgghhbcad@gmail.com>
2022-09-20 21:31:35 -0400

    add an option to allow changing N for random name and password

--- examples/0014.random/name.cc
+++ examples/0014.random/name.cc
@@ -2,8 +2,13 @@
 #include<fast_io_device.h>
 #include<random>
 
-int main()
+int main(int argc,char** argv)
 {
+	std::size_t n{100};
+	if(1<argc)
+	{
+		n=fast_io::to<std::size_t>(argv[1]);
+	}
 	fast_io::u8ibuf_file ibuf(u"freq.txt");
 	fast_io::details::naive_vector<char8_t> vec;
 	{
@@ -21,9 +26,9 @@ int main()
 	std::uniform_int_distribution<std::size_t> ud(0,vec.size()-1);
 	std::uniform_int_distribution<std::size_t> rlen(6,12);
 	fast_io::u8obuf_file obf(u"name.txt");
-	for(std::size_t i(0);i!=1000000;++i)
+	for(std::size_t i{};i!=n;++i)
 	{
-		for(std::size_t j(0),s(rlen(eng));j!=s;++j)
+		for(std::size_t j{},s(rlen(eng));j!=s;++j)
 			print(obf,fast_io::mnp::chvw(vec[ud(eng)]));
 		println(obf);
 	}	
--- examples/0014.random/password.cc
+++ examples/0014.random/password.cc
@@ -2,15 +2,20 @@
 #include<fast_io_device.h>
 #include<random>
 
-int main()
+int main(int argc,char** argv)
 {
+	std::size_t n{100};
+	if(1<argc)
+	{
+		n=fast_io::to<std::size_t>(argv[1]);
+	}
 	fast_io::u8obuf_file obf(u"password.txt");
 	fast_io::ibuf_white_hole_engine eng;
 	std::uniform_int_distribution<std::size_t> ud(0,61);
 	std::uniform_int_distribution<std::size_t> rlen(8,20);
-	for(std::size_t i(0);i!=1000000;++i)
+	for(std::size_t i{};i!=n;++i)
 	{
-		for(std::size_t j(0),s(rlen(eng));j!=s;++j)
+		for(std::size_t j{},s(rlen(eng));j!=s;++j)
 		{
 			char8_t ch(static_cast<char8_t>(ud(eng)));
 			if(ch<10u)
