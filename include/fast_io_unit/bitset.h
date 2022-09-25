10374e5d09345fbb31b48d6f5f7150805db5fb4c
trcrsired <uwgghhbcad@gmail.com>
2022-09-25 00:39:00 -0400

    finish std::bitset output

--- include/fast_io_unit/bitset.h
+++ include/fast_io_unit/bitset.h
@@ -13,11 +13,11 @@ struct bitset_print_reserve_proxy
 #else
 	unsigned long;
 #endif
-	using native_storage_const_pointer = native_storage_type const*
+	using native_storage_const_pointer
 #if __has_cpp_attribute(__gnu__::__may_alias__)
 	[[__gnu__::__may_alias__]]
 #endif
-;
+	= native_storage_type const*;
 	using reference_type = native_storage_const_pointer;
 	native_storage_const_pointer reference{};
 	::std::size_t bits{};
@@ -34,13 +34,13 @@ namespace details
 {
 
 template<::std::integral char_type>
-inline constexpr char_type* pr_vs_iter_impl(char_type* iter,
-	::std::unsigned_integral auto p,std::size_t n) noexcept
+inline constexpr char_type* pr_vs_iter_impl(char_type* first,::std::size_t bits,
+	::std::unsigned_integral auto p) noexcept
 {
-	auto last{iter+n};
-	for(;iter!=last;++iter)
+	auto last{first+bits};
+	for(auto i{last};i!=first;)
 	{
-		*iter=static_cast<char_type>(char_literal_v<u8'0',char_type>+(p&0x1u));
+		*(--i)=static_cast<char_type>(char_literal_v<u8'0',char_type>+(p&0x1u));
 		p>>=1;
 	}
 	return last;
@@ -54,15 +54,12 @@ inline constexpr char_type* prv_rsv_bitset_impl(char_type* iter,typename bitset_
 		::std::size_t bits_per_rep{::std::numeric_limits<nr_type>::digits};
 	::std::size_t const counts{bits/bits_per_rep};
 	::std::size_t const remain_bits{bits%bits_per_rep};
-	auto i{ptr},e{ptr+counts};
-	for(;i!=e;++i)
-	{
-		iter=pr_vs_iter_impl(iter,*i,bits_per_rep);
-	}
+	auto ptr_end{ptr+counts};
 	if(remain_bits)
 	{
-		iter=pr_vs_iter_impl(iter,*e,remain_bits);
+		iter=pr_vs_iter_impl(iter,remain_bits,*ptr_end);
 	}
+	for(;ptr!=ptr_end;iter=pr_vs_iter_impl(iter,bits_per_rep,*(--ptr_end)));
 	return iter;
 }
 
@@ -77,7 +74,7 @@ inline constexpr char_type* print_reserve_define(io_reserve_type_t<char_type,bit
 template<
 	::std::size_t n>
 inline bitset_print_reserve_proxy print_alias_define(io_alias_t,
-	::std::bitset<n> bts) noexcept
+	::std::bitset<n> const& bts) noexcept
 {
 	return ::fast_io::bitset_print_reserve_proxy{reinterpret_cast<::fast_io::bitset_print_reserve_proxy::native_storage_const_pointer>(__builtin_addressof(bts)),n};
 }
