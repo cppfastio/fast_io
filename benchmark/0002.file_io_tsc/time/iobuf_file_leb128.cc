0622566ab29e09392f741a713b6ae72a3a76cd43
havedifficultyinfindingnames <13918219745@163.com>
2022-10-03 17:17:16 +0800

    fix error, add benchmark

---
+++ benchmark/0002.file_io_tsc/time/iobuf_file_leb128.cc
@@ -0,0 +1,26 @@
+#include<fast_io.h>
+#include<fast_io_device.h>
+#include<fast_io_driver/timer.h>
+#include<vector>
+
+int main()
+{
+	constexpr std::size_t N(10000000);
+	{
+	fast_io::timer t(u8"output");
+	fast_io::obuf_file obf(u8"iobuf_file_le32.dat");
+	for(std::size_t i{};i!=N;++i)
+	{
+		print(obf,fast_io::mnp::leb128_put(i));
+	}
+	}
+	std::vector<std::size_t> vec(N,0);
+	{
+	fast_io::timer t(u8"input");
+	fast_io::ibuf_file ibf(u8"iobuf_file_le32.dat");
+	for(auto it{vec.begin()};it!=vec.cend();++it)
+	{
+		scan(ibf,fast_io::mnp::leb128_get(*it));
+	}
+	}
+}
