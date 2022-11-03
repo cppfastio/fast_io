612c163c9fe73d32cfcf6bc6b0ea08d94af0a665
trcrsired <uwgghhbcad@gmail.com>
2022-11-03 05:48:07 -0400

    provide a more fair comparison to std::istream for line_get and scan string with fast_io::filebuf_file

---
+++ benchmark/0010.line/line_get_fbf.cc
@@ -0,0 +1,21 @@
+﻿#include<string>
+#include<fast_io.h>
+#include<fast_io_device.h>
+#include<fast_io_legacy.h>
+#include<fast_io_driver/timer.h>
+#include<vector>
+
+int main()
+{
+	std::size_t lines{};
+	{
+		fast_io::timer t(u8"input");
+		//use std::filebuf_file to provide a fair comparison to C++ stream facilities
+		fast_io::filebuf_file fbf(u8"ibuf_white_hole_engine.txt",fast_io::open_mode::in);
+		for(std::string str;scan<true>(fbf,fast_io::mnp::line_get(str));)
+		{
+			++lines;
+		}
+	}
+	println(lines);
+}
