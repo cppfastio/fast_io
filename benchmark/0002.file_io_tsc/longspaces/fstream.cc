3ac226ccc934f51c1e110b7573023a23da6ecea3
trcrsired <uwgghhbcad@gmail.com>
2022-11-03 14:04:08 -0400

    Optimize base 11-36 input. Optimize input for tons of spaces for numbers Provide new benchmarks for longspaces reading

---
+++ benchmark/0002.file_io_tsc/longspaces/fstream.cc
@@ -0,0 +1,28 @@
+﻿#include<fast_io.h>
+#include<fast_io_device.h>
+#include<fast_io_legacy.h>
+#include<fast_io_driver/timer.h>
+#include<vector>
+#include"longspaces.h"
+
+int main()
+{
+	{
+	fast_io::timer t(u8"output");
+	fast_io::filebuf_file obf(u8"fstream.txt",fast_io::open_mode::out);
+	std::ostream fout(obf.fb);
+	for(std::size_t i{};i!=N;++i)
+	{
+		fout<<i;
+		obf.fb->sputn(filled_chars<char>.data(),static_cast<std::streampos>(filled_chars<char>.size()));
+	}
+	}
+	std::vector<std::size_t> vec(N);
+	{
+	fast_io::timer t(u8"input");
+	fast_io::filebuf_file ibf(u8"fstream.txt",fast_io::open_mode::in);
+	std::istream fin(ibf.fb);
+	for(std::size_t i{};i!=N;++i)
+		fin>>vec[i];
+	}
+}
