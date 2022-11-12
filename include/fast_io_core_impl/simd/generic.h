d183d3afb440830dfd96ab01484fe3bf8de918a3
trcrsired <uwgghhbcad@gmail.com>
2022-11-12 18:04:54 -0500

    correctly implement swap_endian for arm neon on msvc

--- include/fast_io_core_impl/simd/generic.h
+++ include/fast_io_core_impl/simd/generic.h
@@ -248,21 +248,22 @@ simd_vector
 				if constexpr(sizeof(T)==8)
 				{
 					poly8x16_t temp_vec = __builtin_bit_cast(poly8x16_t,*this);
-					temp_vec=vrev16_p8(temp_vec);
+					temp_vec=vrev64q_p8(temp_vec);
 					*this=__builtin_bit_cast(simd_vector<T,N>,temp_vec);
 				}
 				else if constexpr(sizeof(T)==4)
 				{
 					uint32x4_t temp_vec = __builtin_bit_cast(uint32x4_t,*this);
-					temp_vec=vrev64q_u32(temp_vec);
+					temp_vec=vrev32q_p8(temp_vec);
 					*this=__builtin_bit_cast(simd_vector<T,N>,temp_vec);
 				}
 				else if constexpr(sizeof(T)==2)
 				{
 					uint16x8_t temp_vec = __builtin_bit_cast(uint16x8_t,*this);
-					temp_vec=vrev32q_u16(temp_vec);
+					temp_vec=vrev16q_p8(temp_vec);
 					*this=__builtin_bit_cast(simd_vector<T,N>,temp_vec);
 				}
+				return;
 			}
 #endif
 		}
