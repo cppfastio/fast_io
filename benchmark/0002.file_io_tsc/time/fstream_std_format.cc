f7ea335e7c8e34736bb68451f6436b21cbc69732
trcrsired <uwgghhbcad@gmail.com>
2022-11-14 23:38:22 -0500

    Fix benchmark of fstream_std_format.cc

--- benchmark/0002.file_io_tsc/time/fstream_std_format.cc
+++ benchmark/0002.file_io_tsc/time/fstream_std_format.cc
@@ -11,7 +11,7 @@ int main()
 	constexpr std::size_t N(10000000);
 	{
 	fast_io::timer t(u8"output");
-	std::ofstream fout(u8"fstream_std_format.txt");
+	std::ofstream fout("fstream_std_format.txt");
 	for(std::size_t i{};i!=N;++i)
 	{
 		fout<<std::format("{}\n",i);
