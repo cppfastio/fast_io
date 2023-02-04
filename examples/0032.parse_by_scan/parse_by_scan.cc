e38541a7394e34cdb65e8fa4d73b82acbba85af4
trcrsired <uwgghhbcad@gmail.com>
2023-02-04 12:24:23 -0500

    add parse by scan api to allow direct parse data with pointer range

---
+++ examples/0032.parse_by_scan/parse_by_scan.cc
@@ -0,0 +1,14 @@
+#include<fast_io.h>
+
+int main()
+{
+	constexpr char8_t const testbuffer[]{u8"412741298414 1235321525"};
+	::std::uint_least64_t val;
+	auto [position,code] = ::fast_io::parse_by_scan(testbuffer,testbuffer+sizeof(testbuffer),val);
+	if(code!=::fast_io::parse_code::ok)
+	{
+		perr("parse failed\n");
+		return 1;
+	}
+	println("val=",val," position:",position-testbuffer);
+}
