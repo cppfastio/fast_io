6edfa09156df95b6067dce09fd55d5d73ac85e14
trcrsired <uwgghhbcad@gmail.com>
2023-03-02 16:36:49 -0500

    add test for random data

---
+++ benchmark/0002.file_io_tsc/random/c_file.cc
@@ -0,0 +1,24 @@
+#include<fast_io.h>
+#include<fast_io_device.h>
+#include<fast_io_driver/timer.h>
+#include"generate.h"
+
+int main()
+{
+	auto tb{generate_random_data()};
+	{
+	fast_io::timer t(u8"output");
+	fast_io::c_file cf(u8"c_file.txt",::fast_io::open_mode::out);
+	for(auto& e : tb)
+		println(cf,e);;
+	}
+	::fast_io::vector<std::size_t> vec(N);
+	{
+	fast_io::timer t(u8"input");
+	fast_io::c_file cf(u8"c_file.txt",::fast_io::open_mode::in);
+	for(std::size_t i{};i!=N;++i)
+	{
+		scan(cf,vec[i]);
+	}
+	}
+}
