b67c0ddaa48bbe65e521782c92f793b4b780cbf8
trcrsired <uwgghhbcad@gmail.com>
2023-05-08 18:23:49 -0400

    fix modern

--- include/fast_io_core_impl/concepts/stream.h
+++ include/fast_io_core_impl/concepts/stream.h
@@ -3,6 +3,89 @@
 namespace fast_io
 {
 
+template<typename T>
+concept output_stream = ::fast_io::details::has_output_or_io_stream_ref_define<T>;
+
+template<typename T>
+concept input_stream = ::fast_io::details::has_input_or_io_stream_ref_define<T>;
+
+template<typename T>
+concept io_stream = output_stream<T>&&input_stream<T>&&::fast_io::details::has_io_stream_ref_define<T>;
+
+template<typename T>
+concept buffer_output_stream = output_stream<T>&&::fast_io::details::streamreflect::has_obuffer_basic_ops<T>;
+
+template<typename T>
+concept noline_buffer_output_stream = buffer_output_stream<T>;
+
+template<typename T>
+concept buffer_input_stream = input_stream<T>&&::fast_io::details::streamreflect::has_ibuffer_basic_ops<T>;
+
+template<typename T>
+concept buffer_io_stream = io_stream<T>&&buffer_input_stream<T>&&buffer_output_stream<T>;
+
+template<typename T>
+concept contiguous_input_stream = buffer_input_stream<T>&&::fast_io::details::streamreflect::has_ibuffer_underflow_never_define<T>;
+
+template<typename T>
+concept constant_size_buffer_output_stream = buffer_output_stream<T>&&::fast_io::details::streamreflect::has_obuffer_constant_size<T>;
+
+template<typename T>
+concept dynamic_output_stream = buffer_output_stream<T>&&::fast_io::details::streamreflect::has_obuffer_reserve_define<T>;
+
+template<typename T>
+concept random_access_output_stream = output_stream<T>&&::fast_io::details::streamreflect::has_output_stream_seek<T>;
+
+template<typename T>
+concept random_access_input_stream = input_stream<T>&&::fast_io::details::streamreflect::has_input_stream_seek<T>;
+
+template<typename T>
+concept random_access_io_stream = io_stream<T>&&
+	random_access_input_stream<T>&&
+	random_access_output_stream<T>&&
+	::fast_io::details::streamreflect::has_io_stream_seek<T>;
+
+template<typename T>
+concept mutex_output_stream = output_stream<T>&&::fast_io::details::streamreflect::has_output_stream_lock<T>;
+
+template<typename T>
+concept mutex_input_stream = input_stream<T>&&::fast_io::details::streamreflect::has_input_stream_lock<T>;
+
+template<typename T>
+concept mutex_io_stream = io_stream<T>&&
+	mutex_input_stream<T>&&
+	mutex_output_stream<T>&&
+	::fast_io::details::streamreflect::has_io_stream_lock<T>;
+
+
+template<typename T>
+concept byte_output_stream =
+output_stream<T>&&
+(::fast_io::details::streamreflect::has_write_some_bytes_define<T>||
+::fast_io::details::streamreflect::has_write_all_bytes_define<T>||
+::fast_io::details::streamreflect::has_scatter_write_some_bytes_define<T>||
+::fast_io::details::streamreflect::has_scatter_write_all_bytes_define<T>);
+
+template<typename T>
+concept byte_input_stream =
+input_stream<T>&&
+(::fast_io::details::streamreflect::has_read_some_bytes_define<T>||
+::fast_io::details::streamreflect::has_read_all_bytes_define<T>||
+::fast_io::details::streamreflect::has_scatter_read_some_bytes_define<T>||
+::fast_io::details::streamreflect::has_scatter_read_all_bytes_define<T>);
+
+template<typename T>
+concept byte_io_stream = io_stream<T>&&byte_input_stream<T>&&byte_output_stream<T>;
+
+template<typename T>
+concept zero_copy_input_stream =
+input_stream<T>&&::fast_io::details::streamreflect::has_zero_copy_in_handle<T>;
+
+template<typename T>
+concept zero_copy_output_stream =
+output_stream<T>&&::fast_io::details::streamreflect::has_zero_copy_out_handle<T>;
+
+#if 0
 #if 1
 template<typename T>
 concept stream = details::input_stream_impl<T>||details::output_stream_impl<T>;
@@ -182,5 +265,7 @@ concept status_input_stream = requires(T in)
 {
 	{scan_status_define(in)}->std::convertible_to<bool>;
 };
+
+#endif
 
 }
