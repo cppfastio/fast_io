60371c16f055a4966182d95abb9171f7c6c22364
trcrsired <uwgghhbcad@gmail.com>
2023-05-23 03:54:49 -0400

    Add consteval to cur_src_loc manipulator

--- include/fast_io_core_impl/source_location.h
+++ include/fast_io_core_impl/source_location.h
@@ -3,60 +3,90 @@
 namespace fast_io
 {
 
-struct source_location_scatter
+template<::std::integral char_type>
+requires (sizeof(char_type)==1)
+inline constexpr reserve_scatters_size_result print_reserve_scatters_size(::fast_io::io_reserve_type_t<char_type,::std::source_location>) noexcept
 {
-	basic_io_scatter_t<char> file_name;
-	basic_io_scatter_t<char> function_name;
-	std::uint_least32_t line;
-	std::uint_least32_t column;
-};
+	constexpr std::size_t ulint32_rsv_size{print_reserve_size(::fast_io::io_reserve_type<char_type,::std::uint_least32_t>)};
+	constexpr std::size_t total_uint_least32_t_rsv_size{ulint32_rsv_size*2+3};
+	return {3,total_uint_least32_t_rsv_size};
+}
 
 namespace details
 {
 
-inline constexpr std::size_t print_reserve_size_source_location_impl(source_location_scatter location) noexcept
+template<::std::integral char_type>
+requires (sizeof(char_type)==1)
+inline constexpr basic_reserve_scatters_define_result<char_type> prrsv_reserve_scatters_source_location_define_impl(
+	::fast_io::basic_io_scatter_t<char_type>* pscatters,char_type* pbuffer,::std::source_location const& location) noexcept
 {
-	constexpr std::size_t uint32_rsv_size{print_reserve_size(io_reserve_type<char,std::uint_least32_t>)};
-	constexpr std::size_t total_uint_least32_t_rsv_size{uint32_rsv_size*2+3};
-	return intrinsics::add_or_overflow_die_chain(location.file_name.len,location.function_name.len,total_uint_least32_t_rsv_size);
+	using chartypeconstaliasptr
+#if __has_cpp_attribute(__gnu__::__may_alias__)
+	[[__gnu__::__may_alias__]]
+#endif
+	= char_type const*;
+	if constexpr(::std::same_as<char_type,char>)
+	{
+		char const* fnm{location.file_name()};
+		*pscatters=::fast_io::basic_io_scatter_t<char_type>{fnm,::fast_io::cstr_len(fnm)};
+	}
+	else
+	{
+		char_type const* fnm{reinterpret_cast<chartypeconstaliasptr>(location.file_name())};
+		*pscatters=::fast_io::basic_io_scatter_t<char_type>{fnm,::fast_io::cstr_len(fnm)};
+	}
+	++pscatters;
+	char_type *iter{pbuffer};
+	*iter=::fast_io::char_literal_v<u8':',char_type>;
+	++iter;
+	iter=print_reserve_define(::fast_io::io_reserve_type<char_type,::std::uint_least32_t>,iter,location.line());
+	*iter=::fast_io::char_literal_v<u8':',char_type>;
+	++iter;
+	iter=print_reserve_define(::fast_io::io_reserve_type<char_type,::std::uint_least32_t>,iter,location.column());
+	*iter=::fast_io::char_literal_v<u8':',char_type>;
+	++iter;
+	*pscatters=::fast_io::basic_io_scatter_t<char_type>{pbuffer,static_cast<::std::size_t>(iter-pbuffer)};
+	++pscatters;
+	if constexpr(::std::same_as<char_type,char>)
+	{
+		char const* fnm{location.function_name()};
+		*pscatters=::fast_io::basic_io_scatter_t<char_type>{fnm,::fast_io::cstr_len(fnm)};
+	}
+	else
+	{
+		char_type const* fnm{reinterpret_cast<chartypeconstaliasptr>(location.function_name())};
+		*pscatters=::fast_io::basic_io_scatter_t<char_type>{fnm,::fast_io::cstr_len(fnm)};
+	}
+	++pscatters;
+	return {pscatters,iter};
 }
 
-inline constexpr char* print_reserve_define_source_location_impl(char* iter,source_location_scatter location) noexcept
-{
-	*(iter=non_overlapped_copy_n(location.file_name.base,location.file_name.len,iter))=':';
-	*(iter=print_reserve_define(io_reserve_type<char,std::uint_least32_t>,++iter,location.line))=':';
-	*(iter=print_reserve_define(io_reserve_type<char,std::uint_least32_t>,++iter,location.column))=':';
-	return non_overlapped_copy_n(location.function_name.base,location.function_name.len,++iter);
 }
 
-inline constexpr source_location_scatter print_alias_define_source_location_impl(std::source_location location) noexcept
+template<::std::integral char_type>
+requires (sizeof(char_type)==1)
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr basic_reserve_scatters_define_result<char_type> print_reserve_scatters_define(
+	::fast_io::io_reserve_type_t<char_type,::std::source_location>,
+	::fast_io::basic_io_scatter_t<char_type>* pscatters,char_type* pbuffer,::std::source_location const& location) noexcept
 {
-	return {{location.file_name(),cstr_len(location.file_name())},
-	{location.function_name(),cstr_len(location.function_name())},
-	location.line(),location.column()};
-}
-
-}
-
-inline constexpr std::size_t print_reserve_size(io_reserve_type_t<char,source_location_scatter>,source_location_scatter location) noexcept
-{
-	return details::print_reserve_size_source_location_impl(location);
-}
-
-inline constexpr char* print_reserve_define(io_reserve_type_t<char,source_location_scatter>, char* iter,source_location_scatter location) noexcept
-{
-	return details::print_reserve_define_source_location_impl(iter,location);
-}
-
-inline constexpr source_location_scatter print_alias_define(io_alias_t,std::source_location location) noexcept
-{
-	return details::print_alias_define_source_location_impl(location);
+	return ::fast_io::details::prrsv_reserve_scatters_source_location_define_impl(pscatters,pbuffer,location);
 }
 
 namespace manipulators
 {
 
-inline constexpr ::std::source_location cur_src_loc(::std::source_location loc=::std::source_location::current()) noexcept
+inline
+#ifdef __cpp_consteval
+consteval
+#else
+constexpr
+#endif
+::std::source_location cur_src_loc(::std::source_location loc=::std::source_location::current()) noexcept
 {
 	return loc;
 }
