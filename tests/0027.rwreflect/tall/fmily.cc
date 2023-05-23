574fb0814e174da5c67c1a3673bd2bd719548097
trcrsired <uwgghhbcad@gmail.com>
2023-05-23 04:04:50 -0400

    Simplify fmily.cc test

--- tests/0027.rwreflect/tall/fmily.cc
+++ tests/0027.rwreflect/tall/fmily.cc
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include<fast_io_core.h>
 #if __has_include(<sys/uio.h>)
 #include<sys/uio.h>
@@ -7,15 +8,11 @@
 
 struct foo
 {
-	using input_char_type = char;
-	using output_char_type = char;
+	using input_char_type = char8_t;
+	using output_char_type = char8_t;
+	int fd{1};
 };
 
-#if 0
-extern "C" ::std::byte const* fakewrite(
-	::std::byte const* first,
-	::std::byte const* last) noexcept;
-#endif
 inline constexpr foo output_stream_ref_define(foo f) noexcept
 {
 	return f;
@@ -23,9 +20,9 @@ inline constexpr foo output_stream_ref_define(foo f) noexcept
 extern "C" void fake_scatters_write(
 	::fast_io::io_scatter_t const* base,::std::size_t len) noexcept;
 
-inline ::std::byte const* write_some_bytes_overflow_define(foo,::std::byte const* first,::std::byte const* last)
+inline ::std::byte const* write_some_bytes_overflow_define(foo f,::std::byte const* first,::std::byte const* last)
 {
-	auto res{::fast_io::noexcept_call(::write,2,first,static_cast<::std::size_t>(last-first))};
+	auto res{::fast_io::noexcept_call(::write,f.fd,first,static_cast<::std::size_t>(last-first))};
 	if(res<0)
 	{
 		::std::abort();
@@ -34,81 +31,31 @@ inline ::std::byte const* write_some_bytes_overflow_define(foo,::std::byte const
 }
 #if __has_include(<sys/uio.h>)
 inline ::fast_io::io_scatter_status_t scatter_write_some_bytes_overflow_define(
-	foo,
+	foo f,
 	::fast_io::io_scatter_t const* base,::std::size_t len)
 {
-	auto res{::fast_io::noexcept_call(::writev,2,reinterpret_cast<struct iovec const*>(base),len)};
+	auto res{::fast_io::noexcept_call(::writev,f.fd,reinterpret_cast<struct iovec const*>(base),len)};
 	if(res<0)
 	{
 		::std::abort();
 	}
 	return ::fast_io::scatter_size_to_status(static_cast<::std::size_t>(res),base,len);
-//	return fake_scatters_write(base,len);
 }
 #endif
+
+struct bar
+{};
+
+template<::fast_io::output_stream optstmtype>
+inline constexpr void print_define(::fast_io::io_reserve_type_t<char8_t,bar>,optstmtype opt,bar b)
+{
+	::fast_io::operations::print_freestanding(opt,u8"\nbar",1,u8"\nbar2\n",u8"bar3\n");
+}
+
+static_assert(::fast_io::printable<char8_t,bar>);
+
 int main()
 {
-	foo f;
-
-#if 0	
-
-constexpr bool test_char_put
-{
-#ifdef TEST_CHAR_PUT
-true
-#endif
-};
-
-constexpr bool use_fake
-{
-#ifdef USE_FAKE
-true
-#endif
-};
-	if constexpr(test_char_put)
-	{
-		if constexpr(use_fake)
-		{
-			char const ch{'4'};
-			fakewrite(reinterpret_cast<::std::byte const*>(__builtin_addressof(ch)),
-				reinterpret_cast<::std::byte const*>(__builtin_addressof(ch)+1));
-		}
-		else
-		{
-			::fast_io::operations::char_put(f,'4');
-		}
-	}
-	else
-	{
-		char const buffer[]="asfasfsafasf";
-	if constexpr(use_fake)
-	{
-		fakewrite(reinterpret_cast<::std::byte const*>(buffer),
-			reinterpret_cast<::std::byte const*>(buffer+5));
-	}
-	else
-	{
-	::fast_io::operations::write_some(f,buffer,
-		buffer+5);
-	}
-	}
-	char buffer[20]="abcde\nf";
-	char buffer1[20]="ppppp\nf";
-
-	::fast_io::io_scatter_t scat[2]
-	{{buffer,6},{buffer1,6}};
-
-		::fast_io::operations::scatter_write_all_bytes(f,scat,2);
-#endif
-#if TEST_WRITE_ALL
-	::fast_io::operations::write_all(f,"abc","abc"+3);
-#else
-#if 0
-	::fast_io::operations::print_freestanding_decay<true>(f,
-		::fast_io::basic_io_scatter_t<char>{"abc\n",4},
-		::fast_io::basic_io_scatter_t<char>{"abcd\n",5},
-		::fast_io::basic_io_scatter_t<char>{"abcde",5});
-#endif
-	::fast_io::operations::println_freestanding(f,"abc\n","abcsdfasdf\n","asfasf\n","sdgsg",421);
-#endif
+	foo f{1};
+	::fast_io::operations::println_freestanding(f,::std::source_location::current(),u8"\nPara1\n",u8"Para2\n",u8"Para3\n",u8"Para4\nPara",5,u8"\nPara",6,bar{},u8"Para7\n",u8"Para8");
 }
