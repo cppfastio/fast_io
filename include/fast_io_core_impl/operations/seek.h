98a9a5432be836ec2fe5eae217226a96910bd11b
trcrsired <uwgghhbcad@gmail.com>
2023-05-24 18:10:21 -0400

    Add rewind operations for seek

--- include/fast_io_core_impl/operations/seek.h
+++ include/fast_io_core_impl/operations/seek.h
@@ -1,6 +1,9 @@
 #pragma once
 
-namespace fast_io::operations::decay
+namespace fast_io::operations
+{
+
+namespace decay
 {
 
 template<typename T>
@@ -12,13 +15,7 @@ requires (::fast_io::details::has_input_or_io_stream_seek_bytes_define<T>)
 #endif
 inline constexpr ::fast_io::intfpos_t input_stream_seek_bytes_decay(T t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
 {
-	if constexpr(::fast_io::details::has_input_or_io_stream_mutex_ref_define<T>)
-	{
-		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
-		return ::fast_io::operations::decay::input_stream_seek_bytes_decay(
-			::fast_io::operations::decay::input_stream_unlocked_ref_decay(t),off,skd);
-	}
-	else
+	if constexpr(::fast_io::details::has_input_or_io_stream_seek_bytes_define<T>)
 	{
 		if constexpr(::fast_io::details::has_input_or_io_stream_buffer_flush_define<T>)
 		{
@@ -33,10 +30,28 @@ inline constexpr ::fast_io::intfpos_t input_stream_seek_bytes_decay(T t,::fast_i
 			return io_stream_seek_bytes_define(t,off,skd);
 		}
 	}
+	else if constexpr(::fast_io::details::has_input_or_io_stream_mutex_ref_define<T>)
+	{
+		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
+		return ::fast_io::operations::decay::input_stream_seek_bytes_decay(
+			::fast_io::operations::decay::input_stream_unlocked_ref_decay(t),off,skd);
+	}
 }
 
 template<typename T>
-requires (::fast_io::details::has_output_or_io_stream_seek_bytes_define<T>)
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void input_stream_rewind_bytes_decay(T t)
+{
+	::fast_io::operations::decay::input_stream_seek_bytes_decay(t,0,::fast_io::seekdir::beg);
+}
+
+template<typename T>
+requires (::fast_io::details::has_output_or_io_stream_seek_bytes_define<T>||
+	::fast_io::details::has_output_or_io_stream_mutex_ref_define<T>)
 #if __has_cpp_attribute(__gnu__::__always_inline__)
 [[__gnu__::__always_inline__]]
 #elif __has_cpp_attribute(msvc::forceinline)
@@ -44,13 +59,7 @@ requires (::fast_io::details::has_output_or_io_stream_seek_bytes_define<T>)
 #endif
 inline constexpr ::fast_io::intfpos_t output_stream_seek_bytes_decay(T t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
 {
-	if constexpr(::fast_io::details::has_output_or_io_stream_mutex_ref_define<T>)
-	{
-		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
-		return ::fast_io::operations::decay::output_stream_seek_bytes_decay(
-			::fast_io::operations::decay::output_stream_unlocked_ref_decay(t),off,skd);
-	}
-	else
+	if constexpr(::fast_io::details::has_output_or_io_stream_seek_bytes_define<T>)
 	{
 		if constexpr(::fast_io::details::has_output_or_io_stream_buffer_flush_define<T>)
 		{
@@ -65,10 +74,27 @@ inline constexpr ::fast_io::intfpos_t output_stream_seek_bytes_decay(T t,::fast_
 			return io_stream_seek_bytes_define(t,off,skd);
 		}
 	}
+	else if constexpr(::fast_io::details::has_output_or_io_stream_mutex_ref_define<T>)
+	{
+		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
+		return ::fast_io::operations::decay::output_stream_seek_bytes_decay(
+			::fast_io::operations::decay::output_stream_unlocked_ref_decay(t),off,skd);
+	}
 }
 
 template<typename T>
-requires (::fast_io::details::has_output_or_io_stream_seek_bytes_define<T>)
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void output_stream_rewind_bytes_decay(T t)
+{
+	::fast_io::operations::decay::output_stream_seek_bytes_decay(t,0,::fast_io::seekdir::beg);
+}
+
+template<typename T>
+requires (::fast_io::details::has_io_stream_mutex_ref_define<T>)
 #if __has_cpp_attribute(__gnu__::__always_inline__)
 [[__gnu__::__always_inline__]]
 #elif __has_cpp_attribute(msvc::forceinline)
@@ -89,6 +115,16 @@ inline constexpr ::fast_io::intfpos_t io_stream_seek_bytes_decay(T t,::fast_io::
 	}
 }
 
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void io_stream_rewind_bytes_decay(T t)
+{
+	::fast_io::operations::decay::io_stream_seek_bytes_decay(t,0,::fast_io::seekdir::beg);
+}
 
 template<typename T>
 requires (::fast_io::details::has_input_or_io_stream_seek_define<T>)
@@ -99,13 +135,7 @@ requires (::fast_io::details::has_input_or_io_stream_seek_define<T>)
 #endif
 inline constexpr ::fast_io::intfpos_t input_stream_seek_decay(T t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
 {
-	if constexpr(::fast_io::details::has_input_or_io_stream_mutex_ref_define<T>)
-	{
-		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
-		return ::fast_io::operations::decay::input_stream_seek_decay(
-			::fast_io::operations::decay::input_stream_unlocked_ref_decay(t),off,skd);
-	}
-	else
+	if constexpr(::fast_io::details::has_input_or_io_stream_seek_define<T>)
 	{
 		if constexpr(::fast_io::details::has_input_or_io_stream_buffer_flush_define<T>)
 		{
@@ -120,10 +150,28 @@ inline constexpr ::fast_io::intfpos_t input_stream_seek_decay(T t,::fast_io::int
 			return io_stream_seek_define(t,off,skd);
 		}
 	}
+	else if constexpr(::fast_io::details::has_input_or_io_stream_mutex_ref_define<T>)
+	{
+		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
+		return ::fast_io::operations::decay::input_stream_seek_decay(
+			::fast_io::operations::decay::input_stream_unlocked_ref_decay(t),off,skd);
+	}
 }
 
 template<typename T>
-requires (::fast_io::details::has_output_or_io_stream_seek_define<T>)
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void input_stream_rewind_decay(T t)
+{
+	::fast_io::operations::decay::input_stream_seek_decay(t,0,::fast_io::seekdir::beg);
+}
+
+template<typename T>
+requires (::fast_io::details::has_output_or_io_stream_seek_define<T>||
+	::fast_io::details::has_output_or_io_stream_mutex_ref_define<T>)
 #if __has_cpp_attribute(__gnu__::__always_inline__)
 [[__gnu__::__always_inline__]]
 #elif __has_cpp_attribute(msvc::forceinline)
@@ -131,13 +179,7 @@ requires (::fast_io::details::has_output_or_io_stream_seek_define<T>)
 #endif
 inline constexpr ::fast_io::intfpos_t output_stream_seek_decay(T t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
 {
-	if constexpr(::fast_io::details::has_output_or_io_stream_mutex_ref_define<T>)
-	{
-		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
-		return ::fast_io::operations::decay::output_stream_seek_decay(
-			::fast_io::operations::decay::output_stream_unlocked_ref_decay(t),off,skd);
-	}
-	else
+	if constexpr(::fast_io::details::has_output_or_io_stream_seek_define<T>)
 	{
 		if constexpr(::fast_io::details::has_output_or_io_stream_buffer_flush_define<T>)
 		{
@@ -152,10 +194,28 @@ inline constexpr ::fast_io::intfpos_t output_stream_seek_decay(T t,::fast_io::in
 			return io_stream_seek_define(t,off,skd);
 		}
 	}
+	else if constexpr(::fast_io::details::has_output_or_io_stream_mutex_ref_define<T>)
+	{
+		::fast_io::operations::decay::stream_ref_decay_lock_guard lg{::fast_io::operations::decay::input_stream_mutex_ref_decay(t)};
+		return ::fast_io::operations::decay::output_stream_seek_decay(
+			::fast_io::operations::decay::output_stream_unlocked_ref_decay(t),off,skd);
+	}
 }
 
 template<typename T>
-requires (::fast_io::details::has_output_or_io_stream_seek_define<T>)
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void output_stream_rewind_decay(T t)
+{
+	::fast_io::operations::decay::output_stream_seek_decay(t,0,::fast_io::seekdir::beg);
+}
+
+template<typename T>
+requires (::fast_io::details::has_io_stream_mutex_ref_define<T>||
+	::fast_io::details::has_io_stream_seek_define<T>)
 #if __has_cpp_attribute(__gnu__::__always_inline__)
 [[__gnu__::__always_inline__]]
 #elif __has_cpp_attribute(msvc::forceinline)
@@ -171,12 +231,160 @@ inline constexpr ::fast_io::intfpos_t io_stream_seek_decay(T t,::fast_io::intfpo
 	}
 	else
 	{
-		if constexpr(::fast_io::details::has_io_stream_buffer_flush_define<T>)
-		{
-			io_stream_buffer_flush_define(t);
-		}
+		io_stream_buffer_flush_define(t);
 		return io_stream_seek_define(t,off,skd);
 	}
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void io_stream_rewind_decay(T t)
+{
+	::fast_io::operations::decay::io_stream_seek_decay(t,0,::fast_io::seekdir::beg);
+}
+
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr ::fast_io::intfpos_t input_stream_seek_bytes(T&& t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
+{
+	return ::fast_io::operations::decay::input_stream_seek_bytes_decay(::fast_io::manipulators::input_stream_ref(t),
+		off,skd);
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr ::fast_io::intfpos_t output_stream_seek_bytes(T&& t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
+{
+	return ::fast_io::operations::decay::output_stream_seek_bytes_decay(::fast_io::manipulators::output_stream_ref(t),
+		off,skd);
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr ::fast_io::intfpos_t io_stream_seek_bytes(T&& t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
+{
+	return ::fast_io::operations::decay::io_stream_seek_bytes_decay(::fast_io::manipulators::io_stream_ref(t),
+		off,skd);
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr ::fast_io::intfpos_t input_stream_seek(T&& t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
+{
+	return ::fast_io::operations::decay::input_stream_seek_decay(::fast_io::manipulators::input_stream_ref(t),
+		off,skd);
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr ::fast_io::intfpos_t output_stream_seek(T&& t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
+{
+	return ::fast_io::operations::decay::output_stream_seek_decay(::fast_io::manipulators::output_stream_ref(t),
+		off,skd);
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr ::fast_io::intfpos_t io_stream_seek(T&& t,::fast_io::intfpos_t off,::fast_io::seekdir skd)
+{
+	return ::fast_io::operations::decay::io_stream_seek_decay(::fast_io::manipulators::io_stream_ref(t),
+		off,skd);
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void input_stream_rewind_bytes(T&& t)
+{
+	::fast_io::operations::decay::input_stream_rewind_bytes_decay(::fast_io::manipulators::input_stream_ref(t));
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void output_stream_rewind_bytes(T&& t)
+{
+	::fast_io::operations::decay::output_stream_rewind_bytes_decay(::fast_io::manipulators::output_stream_ref(t));
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void io_stream_rewind_bytes(T&& t)
+{
+	::fast_io::operations::decay::io_stream_rewind_bytes_decay(::fast_io::manipulators::io_stream_ref(t));
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void input_stream_rewind(T&& t)
+{
+	::fast_io::operations::decay::input_stream_rewind_decay(::fast_io::manipulators::input_stream_ref(t));
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void output_stream_rewind(T&& t)
+{
+	::fast_io::operations::decay::output_stream_rewind_decay(::fast_io::manipulators::output_stream_ref(t));
+}
+
+template<typename T>
+#if __has_cpp_attribute(__gnu__::__always_inline__)
+[[__gnu__::__always_inline__]]
+#elif __has_cpp_attribute(msvc::forceinline)
+[[msvc::forceinline]]
+#endif
+inline constexpr void io_stream_rewind(T&& t)
+{
+	::fast_io::operations::decay::io_stream_rewind_decay(::fast_io::manipulators::io_stream_ref(t));
 }
 
 }
