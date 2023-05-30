03ac682a4c221d231302243389b98c6515ba32a1
trcrsired <uwgghhbcad@gmail.com>
2023-05-30 04:02:31 -0400

    move on io_deco_filter

--- include/fast_io_freestanding.h
+++ include/fast_io_freestanding.h
@@ -27,6 +27,7 @@
 #include"fast_io_unit/floating/impl.h"
 #endif
 #include"fast_io_freestanding_impl/io_buffer/impl.h"
+#include"fast_io_freestanding_impl/io_deco_filter/impl.h"
 #include"fast_io_freestanding_impl/auto_indent.h"
 #include"fast_io_freestanding_impl/serializations/impl.h"
 #include"fast_io_freestanding_impl/space_reserve.h"
