c15a923a69610cfbeac22b6863cf9076561afa24
trcrsired <uwgghhbcad@gmail.com>
2023-07-13 12:27:31 -0400

    fix pulseaudio

--- include/fast_io_driver/pulseaudio.h
+++ include/fast_io_driver/pulseaudio.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 #include <pulse/pulseaudio.h>
 #include <pulse/simple.h>
-#include"../fast_io_hosted.h"
+#include"../fast_io_freestanding.h"
 
 namespace fast_io
 {
@@ -11,12 +11,6 @@ class pulse_audio_error
 public:
 	int error{};
 	pulse_audio_error(int e=PA_OK):error(e){}
-#if 0
-	void report(error_reporter& err) const override
-	{
-		print_freestanding(err,manipulators::os_c_str(pa_strerror(error)));
-	}
-#endif
 };
 
 inline void throw_pulse_audio_error(int ec=PA_OK)
@@ -33,13 +27,11 @@ class basic_pulseaudio_simple_io_observer
 {
 public:
 	using char_type = ch_type;
+	using output_char_type = char_type;
+	using input_char_type = char_type;
 	using native_handle_type = pa_simple*;
 	native_handle_type s{};
-	constexpr auto& native_handle() const noexcept
-	{
-		return s;
-	}
-	constexpr auto& native_handle() noexcept
+	constexpr native_handle_type native_handle() const noexcept
 	{
 		return s;
 	}
@@ -71,63 +63,89 @@ public:
 		pa_buffer_attr const * 	attr)
 	{
 		int err{};
-		this->native_handle()=pa_simple_new(server,name,dir,dev,stream_name,ss,map,attr,__builtin_addressof(err));
-		if(this->native_handle()==nullptr)
+		this->s=::fast_io::noexcept_call(pa_simple_new,server,name,dir,dev,stream_name,ss,map,attr,__builtin_addressof(err));
+		if(this->s==nullptr)
 			throw_pulse_audio_error(err);
 	}
 	basic_pulseaudio_simple_file(basic_pulseaudio_simple_file const&)=delete;
 	basic_pulseaudio_simple_file& operator=(basic_pulseaudio_simple_file const&)=delete;
-	constexpr basic_pulseaudio_simple_file(basic_pulseaudio_simple_file&& __restrict hd) noexcept:basic_pulseaudio_simple_file(hd.native_handle())
+	constexpr basic_pulseaudio_simple_file(basic_pulseaudio_simple_file&& __restrict hd) noexcept:basic_pulseaudio_simple_file(hd.s)
 	{
-		hd.native_handle()=nullptr;
+		hd.s=nullptr;
 	}
 	basic_pulseaudio_simple_file& operator=(basic_pulseaudio_simple_file&& __restrict hd) noexcept
 	{
-		if(this->native_handle())[[likely]]
-			pa_simple_free(this->native_handle());
-		this->native_handle()=hd.native_handle();
-		hd.native_handle()=nullptr;
+		if(this->s)[[likely]]
+			::fast_io::noexcept_call(pa_simple_free,this->s);
+		this->s=hd.s;
+		hd.s=nullptr;
 		return *this;
 	}
 	inline void reset(native_handle_type newhandle=nullptr) noexcept
 	{
-		if(this->native_handle())[[likely]]
-			pa_simple_free(this->native_handle());
-		this->native_handle()=newhandle;
+		if(this->s)[[likely]]
+			::fast_io::noexcept_call(pa_simple_free,this->s);
+		this->s=newhandle;
 	}
 	~basic_pulseaudio_simple_file()
 	{
-		if(this->native_handle())[[likely]]
-			pa_simple_free(this->native_handle());
+		if(this->s)[[likely]]
+			::fast_io::noexcept_call(pa_simple_free,this->s);
 	}
 };
 
-template<std::integral char_type,::std::contiguous_iterator Iter>
-inline Iter read(basic_pulseaudio_simple_io_observer<char_type> bpsiob,Iter begin,Iter end)
+namespace details
+{
+
+inline void pa_simple_read_underflow_impl(pa_simple *ps,::std::byte *first,::std::byte *last)
 {
 	int err{};
-	if(pa_simple_read(bpsiob.native_handle(),::std::to_address(begin),sizeof(*begin)*(end-begin),__builtin_addressof(err))<0)[[unlikely]]
+	if(::fast_io::noexcept_call(pa_simple_read,ps,first,static_cast<::std::size_t>(last-first),__builtin_addressof(err))<0)[[unlikely]]
 		throw_pulse_audio_error(err);
-	return end;
 }
 
-template<std::integral char_type,::std::contiguous_iterator Iter>
-inline Iter write(basic_pulseaudio_simple_io_observer<char_type> bpsiob,Iter begin,Iter end)
+inline void pa_simple_write_overflow_impl(pa_simple *ps,::std::byte const *first,::std::byte const *last)
 {
 	int err{};
-	if(pa_simple_write(bpsiob.native_handle(),::std::to_address(begin),sizeof(*begin)*(end-begin),__builtin_addressof(err))<0)[[unlikely]]
+	if(::fast_io::noexcept_call(pa_simple_write,ps,first,static_cast<::std::size_t>(last-first),__builtin_addressof(err))<0)[[unlikely]]
 		throw_pulse_audio_error(err);
-	return end;
 }
 
+}
 
-template<std::integral char_type,::std::contiguous_iterator Iter>
+template<std::integral ch_type>
+inline void read_all_bytes_underflow_define(basic_pulseaudio_simple_io_observer<ch_type> iob,::std::byte *first,::std::byte *last)
+{
+	return ::fast_io::details::pa_simple_read_underflow_impl(iob.s,first,last);
+}
+
+template<std::integral ch_type>
+inline void write_all_bytes_overflow_define(basic_pulseaudio_simple_io_observer<ch_type> iob,::std::byte const *first,::std::byte const *last)
+{
+	return ::fast_io::details::pa_simple_write_overflow_impl(iob.s,first,last);
+}
+
+template<std::integral ch_type>
+inline constexpr basic_pulseaudio_simple_io_observer<ch_type> io_stream_ref_define(basic_pulseaudio_simple_io_observer<ch_type> other) noexcept
+{
+	return other;
+}
+
+template<std::integral ch_type>
+inline constexpr basic_pulseaudio_simple_io_observer<char> io_bytes_stream_ref_define(basic_pulseaudio_simple_io_observer<ch_type> other) noexcept
+{
+	return other;
+}
+
+#if 0
+template<std::integral char_type>
 inline void flush(basic_pulseaudio_simple_io_observer<char_type> bpsiob)
 {
 	int err{};
-	if(pa_simple_flush(bpsiob.native_handle(),__builtin_addressof(err))<0)[[unlikely]]
+	if(pa_simple_flush(bpsiob.s,__builtin_addressof(err))<0)[[unlikely]]
 		throw_pulse_audio_error(err);
 }
+#endif
 
 using pulseaudio_simple_io_observer = basic_pulseaudio_simple_io_observer<char>;
 using pulseaudio_simple_file = basic_pulseaudio_simple_file<char>;
