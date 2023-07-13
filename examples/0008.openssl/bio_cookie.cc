e2ab812ee880be9431b8b724f293feeaa885dd14
trcrsired <uwgghhbcad@gmail.com>
2023-07-13 04:12:16 -0400

    add a bio cookie example

---
+++ examples/0008.openssl/bio_cookie.cc
@@ -0,0 +1,11 @@
+﻿#include<fast_io.h>
+#include<fast_io_driver/openssl_driver.h>
+
+using namespace fast_io::io;
+
+int main()
+{
+	fast_io::bio_file bf(fast_io::io_cookie_type<fast_io::native_file>,"bio_cookie.txt",fast_io::open_mode::out);
+	print(bf,"Hello World to bio from fast_io\n");
+	BIO_printf(bf.bio,"hello %s\n","world from openssl bio");
+}
