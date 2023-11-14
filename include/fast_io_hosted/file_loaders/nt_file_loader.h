6ce32493951993dbf46c4192d2834325eb1dadeb
MacroModel <liyn_xlfd@163.com>
2023-11-14 22:40:43 +0800

    format nt_file_loader.h

--- include/fast_io_hosted/file_loaders/nt_file_loader.h
+++ include/fast_io_hosted/file_loaders/nt_file_loader.h
@@ -6,13 +6,15 @@ namespace fast_io
 namespace win32::nt::details 
 {
 
-struct nt_file_loader_return_value_t {
+struct nt_file_loader_return_value_t 
+{
 	char* address_begin{};
 	char* address_end{};
 };
 
 template <::fast_io::nt_family family>
-inline void* nt_create_section_common_impl(void* hfilemappingobj) {
+inline void* nt_create_section_common_impl(void* hfilemappingobj) 
+{
 	::fast_io::win32::nt::object_attributes objAttr{
 		.Length = sizeof(object_attributes),
 		.Attributes = 0x40L /*OBJ_CASE_INSENSITIVE*/
@@ -25,7 +27,8 @@ inline void* nt_create_section_common_impl(void* hfilemappingobj) {
 }
 
 template <::fast_io::nt_family family>
-inline nt_file_loader_return_value_t nt_create_map_view_common_impl(void* handle) {
+inline nt_file_loader_return_value_t nt_create_map_view_common_impl(void* handle) 
+{
 	void* h_section{nt_create_section_common_impl<family>(handle)};
 	::fast_io::basic_nt_family_file<family, char> map_hd{h_section};
 	void* p_map_address{};
@@ -40,37 +43,43 @@ inline nt_file_loader_return_value_t nt_create_map_view_common_impl(void* handle
 }
 
 template <::fast_io::nt_family family>
-inline auto nt_load_file_impl(nt_fs_dirent fsdirent, ::fast_io::open_mode om, ::fast_io::perms pm) {
+inline auto nt_load_file_impl(nt_fs_dirent fsdirent, ::fast_io::open_mode om, ::fast_io::perms pm) 
+{
 	::fast_io::basic_nt_family_file<family, char> nf(fsdirent, om, pm);
 	return nt_create_map_view_common_impl<family>(nf.handle);
 }
 
 template <::fast_io::nt_family family, ::fast_io::constructible_to_os_c_str T>
-inline auto nt_load_file_impl(T const& str, ::fast_io::open_mode om, ::fast_io::perms pm) {
+inline auto nt_load_file_impl(T const& str, ::fast_io::open_mode om, ::fast_io::perms pm) 
+{
 	::fast_io::basic_nt_family_file<family, char> nf(str, om, pm);
 	return nt_create_map_view_common_impl<family>(nf.handle);
 }
 
 template <::fast_io::nt_family family, ::fast_io::constructible_to_os_c_str T>
-inline auto nt_load_file_impl(::fast_io::nt_at_entry ent, T const& str, ::fast_io::open_mode om, ::fast_io::perms pm) {
+inline auto nt_load_file_impl(::fast_io::nt_at_entry ent, T const& str, ::fast_io::open_mode om, ::fast_io::perms pm) 
+{
 	::fast_io::basic_nt_family_file<family, char> nf(ent, str, om, pm);
 	return nt_create_map_view_common_impl<family>(nf.handle);
 }
 
 template <::fast_io::nt_family family, ::fast_io::constructible_to_os_c_str T>
-inline auto nt_load_file_impl(::fast_io::io_kernel_t, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) {
+inline auto nt_load_file_impl(::fast_io::io_kernel_t, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) 
+{
 	::fast_io::basic_nt_family_file<family, char> nf(::fast_io::io_kernel, t, om, pm);
 	return nt_create_map_view_common_impl<family>(nf.handle);
 }
 
 template <::fast_io::nt_family family, ::fast_io::constructible_to_os_c_str T>
-inline auto nt_load_file_impl(::fast_io::io_kernel_t, ::fast_io::nt_at_entry ent, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) {
+inline auto nt_load_file_impl(::fast_io::io_kernel_t, ::fast_io::nt_at_entry ent, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) 
+{
 	::fast_io::basic_nt_family_file<family, char> nf(::fast_io::io_kernel, ent, t, om, pm);
 	return nt_create_map_view_common_impl<family>(nf.handle);
 }
 
 template <::fast_io::nt_family family>
-inline void nt_unload_address(void* address) noexcept {
+inline void nt_unload_address(void* address) noexcept
+{
 	if (address) {
 		void* current_process_handle{reinterpret_cast<void*>(-1)};
 		::fast_io::win32::nt::nt_unmap_view_of_section<family == ::fast_io::nt_family::zw>(current_process_handle, address);
@@ -78,9 +87,11 @@ inline void nt_unload_address(void* address) noexcept {
 }
 
 }  // namespace win32::nt::details
+
 template <::fast_io::nt_family family>
 	requires(family == ::fast_io::nt_family::nt || family == ::fast_io::nt_family::zw)
-class nt_family_file_loader {
+class nt_family_file_loader 
+{
 public:
 	using value_type = char;
 	using pointer = char*;
@@ -98,110 +109,138 @@ public:
 	pointer address_end{};
 
 	inline constexpr nt_family_file_loader() noexcept = default;
-	inline explicit nt_family_file_loader(::fast_io::nt_at_entry ent) {
+	inline explicit nt_family_file_loader(::fast_io::nt_at_entry ent) 
+	{
 		auto ret{::fast_io::win32::nt::details::nt_create_map_view_common_impl<family>(ent.handle)};
 		address_begin = ret.address_begin;
 		address_end = ret.address_end;
 	}
-	inline explicit nt_family_file_loader(::fast_io::nt_fs_dirent fsdirent, ::fast_io::open_mode om = ::fast_io::open_mode::in, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) {
+	inline explicit nt_family_file_loader(::fast_io::nt_fs_dirent fsdirent, ::fast_io::open_mode om = ::fast_io::open_mode::in, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) 
+	{
 		auto ret{::fast_io::win32::nt::details::nt_load_file_impl<family>(fsdirent, om, pm)};
 		address_begin = ret.address_begin;
 		address_end = ret.address_end;
 	}
 	template <::fast_io::constructible_to_os_c_str T>
-	inline explicit nt_family_file_loader(T const& filename, ::fast_io::open_mode om = ::fast_io::open_mode::in, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) {
+	inline explicit nt_family_file_loader(T const& filename, ::fast_io::open_mode om = ::fast_io::open_mode::in, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) 
+	{
 		auto ret{::fast_io::win32::nt::details::nt_load_file_impl<family>(filename, om, pm)};
 		address_begin = ret.address_begin;
 		address_end = ret.address_end;
 	}
 	template <::fast_io::constructible_to_os_c_str T>
-	inline explicit nt_family_file_loader(::fast_io::nt_at_entry ent, T const& filename, ::fast_io::open_mode om = ::fast_io::open_mode::in, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) {
+	inline explicit nt_family_file_loader(::fast_io::nt_at_entry ent, T const& filename, ::fast_io::open_mode om = ::fast_io::open_mode::in, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) 
+	{
 		auto ret{::fast_io::win32::nt::details::nt_load_file_impl<family>(ent, filename, om, pm)};
 		address_begin = ret.address_begin;
 		address_end = ret.address_end;
 	}
 	template <::fast_io::constructible_to_os_c_str T>
-	inline explicit nt_family_file_loader(::fast_io::io_kernel_t, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) {
+	inline explicit nt_family_file_loader(::fast_io::io_kernel_t, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436))
+	{
 		auto ret{::fast_io::win32::nt::details::nt_load_file_impl<family>(::fast_io::io_kernel, t, om, pm)};
 		address_begin = ret.address_begin;
 		address_end = ret.address_end;
 	}
 	template <::fast_io::constructible_to_os_c_str T>
-	inline explicit nt_family_file_loader(::fast_io::io_kernel_t, ::fast_io::nt_at_entry ent, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) {
+	inline explicit nt_family_file_loader(::fast_io::io_kernel_t, ::fast_io::nt_at_entry ent, T const& t, ::fast_io::open_mode om, ::fast_io::perms pm = static_cast<::fast_io::perms>(436)) 
+	{
 		auto ret{::fast_io::win32::nt::details::nt_load_file_impl<family>(::fast_io::io_kernel, ent, t, om, pm)};
 		address_begin = ret.address_begin;
 		address_end = ret.address_end;
 	}
 	nt_family_file_loader(nt_family_file_loader const&) = delete;
 	nt_family_file_loader& operator=(nt_family_file_loader const&) = delete;
-	constexpr nt_family_file_loader(nt_family_file_loader&& __restrict other) noexcept : address_begin(other.address_begin), address_end(other.address_end) {
+	constexpr nt_family_file_loader(nt_family_file_loader&& __restrict other) noexcept : address_begin(other.address_begin), address_end(other.address_end)
+	{
 		other.address_end = other.address_begin = nullptr;
 	}
-	nt_family_file_loader& operator=(nt_family_file_loader&& __restrict other) noexcept {
+	nt_family_file_loader& operator=(nt_family_file_loader&& __restrict other) noexcept 
+	{
 		::fast_io::win32::nt::details::nt_unload_address<family>(address_begin);
 		address_begin = other.address_begin;
 		address_end = other.address_end;
 		other.address_end = other.address_begin = nullptr;
 		return *this;
 	}
-	constexpr pointer data() const noexcept {
+	constexpr pointer data() const noexcept
+	{
 		return address_begin;
 	}
-	constexpr bool empty() const noexcept {
+	constexpr bool empty() const noexcept 
+	{
 		return address_begin == address_end;
 	}
-	constexpr ::std::size_t size() const noexcept {
+	constexpr ::std::size_t size() const noexcept 
+	{
 		return static_cast<::std::size_t>(address_end - address_begin);
 	}
-	constexpr ::std::size_t max_size() const noexcept {
+	constexpr ::std::size_t max_size() const noexcept
+	{
 		return SIZE_MAX;
 	}
-	constexpr const_iterator cbegin() const noexcept {
+	constexpr const_iterator cbegin() const noexcept
+	{
 		return address_begin;
 	}
-	constexpr const_iterator begin() const noexcept {
+	constexpr const_iterator begin() const noexcept
+	{
 		return address_begin;
 	}
-	constexpr iterator begin() noexcept {
+	constexpr iterator begin() noexcept
+	{
 		return address_begin;
 	}
-	constexpr const_iterator cend() const noexcept {
+	constexpr const_iterator cend() const noexcept
+	{
 		return address_end;
 	}
-	constexpr const_iterator end() const noexcept {
+	constexpr const_iterator end() const noexcept
+	{
 		return address_end;
 	}
-	constexpr iterator end() noexcept {
+	constexpr iterator end() noexcept
+	{
 		return address_end;
 	}
-	constexpr const_reverse_iterator crbegin() const noexcept {
+	constexpr const_reverse_iterator crbegin() const noexcept
+	{
 		return const_reverse_iterator{address_end};
 	}
-	constexpr reverse_iterator rbegin() noexcept {
+	constexpr reverse_iterator rbegin() noexcept
+	{
 		return reverse_iterator{address_end};
 	}
-	constexpr const_reverse_iterator rbegin() const noexcept {
+	constexpr const_reverse_iterator rbegin() const noexcept
+	{
 		return const_reverse_iterator{address_end};
 	}
-	constexpr const_reverse_iterator crend() const noexcept {
+	constexpr const_reverse_iterator crend() const noexcept
+	{
 		return const_reverse_iterator{address_begin};
 	}
-	constexpr reverse_iterator rend() noexcept {
+	constexpr reverse_iterator rend() noexcept
+	{
 		return reverse_iterator{address_begin};
 	}
-	constexpr const_reverse_iterator rend() const noexcept {
+	constexpr const_reverse_iterator rend() const noexcept
+	{
 		return const_reverse_iterator{address_begin};
 	}
-	constexpr const_reference front() const noexcept {
+	constexpr const_reference front() const noexcept
+	{
 		return *address_begin;
 	}
-	constexpr reference front() noexcept {
+	constexpr reference front() noexcept
+	{
 		return *address_begin;
 	}
-	constexpr const_reference back() const noexcept {
+	constexpr const_reference back() const noexcept
+	{
 		return address_end[-1];
 	}
-	constexpr reference back() noexcept {
+	constexpr reference back() noexcept
+	{
 		return address_end[-1];
 	}
 
@@ -209,28 +248,34 @@ public:
 	[[nodiscard]]
 #endif
 	constexpr pointer
-	release() noexcept {
+	release() noexcept
+	{
 		pointer temp{this->address_begin};
 		this->address_end = this->address_begin = nullptr;
 		return temp;
 	}
-	inline constexpr reference operator[](size_type size) noexcept {
+	inline constexpr reference operator[](size_type size) noexcept
+	{
 		return address_begin[size];
 	}
-	inline constexpr const_reference operator[](size_type size) const noexcept {
+	inline constexpr const_reference operator[](size_type size) const noexcept
+	{
 		return address_begin[size];
 	}
-	inline void close() {
+	inline void close()
+	{
 		::fast_io::win32::nt::details::nt_unload_address<family>(address_begin);
 		address_end = address_begin = nullptr;
 	}
-	~nt_family_file_loader() {
+	~nt_family_file_loader()
+	{
 		::fast_io::win32::nt::details::nt_unload_address<family>(address_begin);
 	}
 };
 
 template <::fast_io::nt_family family>
-inline constexpr basic_io_scatter_t<char> print_alias_define(::fast_io::io_alias_t, nt_family_file_loader<family> const& load) noexcept {
+inline constexpr basic_io_scatter_t<char> print_alias_define(::fast_io::io_alias_t, nt_family_file_loader<family> const& load) noexcept
+{
 	return {load.data(), load.size()};
 }
 
