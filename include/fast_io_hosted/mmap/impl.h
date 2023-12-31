1f7be51f74f1ba5a4a01a7a6ee249c35959ce7ab
trcrsired <uwgghhbcad@gmail.com>
2023-12-31 17:44:29 -0500

    add sys/mman.h

--- include/fast_io_hosted/mmap/impl.h
+++ include/fast_io_hosted/mmap/impl.h
@@ -6,6 +6,9 @@
 #endif
 
 #if (!defined(_WIN32) || defined(__CYGWIN__) || defined(__WINE__) || defined(__BIONIC__)) && !defined(__MSDOS__) && !defined(_PICOLIBC) && (!defined(__wasi__) || defined(_WASI_EMULATED_MMAN))
+#if __has_include(<sys/mman.h>)
+#include<sys/mman.h>
+#endif
 #include"posix_options.h"
 #include"posix.h"
 #endif
