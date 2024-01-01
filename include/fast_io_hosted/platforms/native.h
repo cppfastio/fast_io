2fcf090d4a3aa6662ff947fb6dac36f9df6641da
MacroModel <liyn_xlfd@163.com>
2024-01-01 14:40:27 +0800

    fix

--- include/fast_io_hosted/platforms/native.h
+++ include/fast_io_hosted/platforms/native.h
@@ -2,7 +2,6 @@
 
 #include"native_base.h"
 
-#if 0
 #if defined(_WIN32) || defined(__CYGWIN__)
 #include"nt_mapping.h"
 #include"win32_mapping.h"
@@ -13,5 +12,4 @@
 #elif !defined(__NEWLIB__) && !defined(__MSDOS__) && (!defined(__wasm__) || (defined(__wasi__)&&defined(_WASI_EMULATED_MMAN))) && __has_include(<sys/mman.h>)
 #include"posix_mapping.h"
 #include"omap.h"
-#endif
 #endif
