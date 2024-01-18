ca87da96ede8cf32c8a8f4e357768d422f0da339
trcrsired <uwgghhbcad@gmail.com>
2024-01-18 03:32:31 -0500

    fix macro guard

--- include/fast_io_hosted/process_revamp/native.h
+++ include/fast_io_hosted/process_revamp/native.h
@@ -2,19 +2,26 @@
 
 #if !defined(__MSDOS__) && !defined(__NEWLIB__) && !defined(__wasi__) && !defined(_PICOLIBC__)
 #include"base.h"
-#if defined(_WIN32) && !defined(_WIN32_WINDOWS)
+#if (defined(_WIN32) && !defined(_WIN32_WINDOWS) &&  !defined(__WINE__)) || defined(__CYGWIN__)
 #include"nt.h"
-namespace fast_io {
-using native_process_observer = ::fast_io::nt_process_observer;
-using native_process = ::fast_io::nt_process;
-}  // namespace fast_io
+#endif
 
-#elif __has_include(<sys/wait.h>)
+#if defined(__CYGWIN__) || ((!defined(_WIN32) || defined(__WINE__)) && __has_include(<sys/wait.h>))
 #include <sys/wait.h>
 #include"posix.h"
-namespace fast_io {
+#endif
+
+namespace fast_io
+{
+#if defined(_WIN32) && !defined(__CYGWIN__) && !defined(__WINE__)
+#if !defined(_WIN32_WINDOWS)
+using native_process_observer = ::fast_io::nt_process_observer;
+using native_process = ::fast_io::nt_process;
+#endif
+#else
 using native_process_observer = ::fast_io::posix_process_observer;
 using native_process = ::fast_io::posix_process;
-}  // namespace fast_io
 #endif
+}  // namespace fast_io
+
 #endif
