938f5143d383cb8249d9a364945a719476709a0d
trcrsired <uwgghhbcad@gmail.com>
2024-03-13 23:21:22 -0400

    shared_ptr_insert

--- tests/0026.container/0001.vector/shared_ptr.cc
+++ tests/0026.container/0001.vector/shared_ptr.cc
@@ -1,20 +1,24 @@
-#include<fast_io_dsal/vector.h>
-#include<fast_io.h>
-#include<memory>
+#include <fast_io_dsal/vector.h>
+#include <fast_io.h>
+#include <memory>
 
 namespace fast_io::freestanding
 {
 
-template<typename T>
+template <typename T>
 struct is_trivially_relocatable<::std::shared_ptr<T>>
 {
 	static inline constexpr bool value = true;
 };
 
-}
+} // namespace fast_io::freestanding
 
 int main()
 {
-	::fast_io::vector<::std::shared_ptr<int>> vec{ std::make_shared<int>(1), std::make_shared<int>(2), std::make_shared<int>(3) };
-	vec.insert(vec.begin()+1, std::make_shared<int>(4));
+	::fast_io::vector<::std::shared_ptr<int>> vec{std::make_shared<int>(1), std::make_shared<int>(2), std::make_shared<int>(3)};
+	vec.insert(vec.begin() + 1, std::make_shared<int>(4));
+	for (auto const &e : vec)
+	{
+		println(::fast_io::mnp::pointervw(e.get()), " use_count: ", e.use_count());
+	}
 }
