563ccbf31c24ce8ab7cfae26af1797fb9839e4b5
trcrsired <uwgghhbcad@gmail.com>
2024-03-15 15:51:59 -0400

    use more extreme

--- benchmark/0011.containers/vector/0002.multi_push_back/main.h
+++ benchmark/0011.containers/vector/0002.multi_push_back/main.h
@@ -4,7 +4,7 @@
 int main()
 {
 	fast_io::timer t(BENCH_VECTOR_COMMENT_STRING);
-	for (std::size_t j{}; j != 100000; ++j)
+	for (std::size_t j{}; j != 50; ++j)
 	{
 		test::vector<std::int_least32_t> vec1;
 		test::vector<char32_t> vec2;
@@ -12,7 +12,14 @@ int main()
 		test::vector<float> vec4;
 		test::vector<char> vec5;
 		test::vector<unsigned char> vec6;
-		for (std::size_t i{}; i != 100; ++i)
+		test::vector<size_t> vec7;
+		test::vector<int8_t> vec8;
+		test::vector<uint8_t> vec9;
+		test::vector<char8_t> vec10;
+		test::vector<char16_t> vec11;
+		test::vector<wchar_t> vec12;
+		test::vector<char32_t> vec13;
+		for (std::size_t i{}; i != 100000; ++i)
 		{
 			vec1.push_back(42);
 			vec2.push_back(42);
@@ -20,48 +27,104 @@ int main()
 			vec4.push_back(43);
 			vec5.push_back(43);
 			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 			vec1.push_back(42);
 			vec2.push_back(42);
 			vec3.push_back(43);
 			vec4.push_back(43);
 			vec5.push_back(43);
 			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 			vec1.push_back(42);
 			vec2.push_back(42);
 			vec3.push_back(43);
 			vec4.push_back(43);
 			vec5.push_back(43);
 			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 			vec1.push_back(42);
 			vec2.push_back(42);
 			vec3.push_back(43);
 			vec4.push_back(43);
 			vec5.push_back(43);
 			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 			vec1.push_back(42);
 			vec2.push_back(42);
 			vec3.push_back(43);
 			vec4.push_back(43);
 			vec5.push_back(43);
 			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 			vec1.push_back(42);
 			vec2.push_back(42);
 			vec3.push_back(43);
 			vec4.push_back(43);
 			vec5.push_back(43);
 			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 			vec1.push_back(42);
 			vec2.push_back(42);
 			vec3.push_back(43);
 			vec4.push_back(43);
 			vec5.push_back(43);
 			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 			vec1.push_back(42);
 			vec2.push_back(42);
 			vec3.push_back(43);
 			vec4.push_back(43);
-			vec5.push_back(44);
-			vec6.push_back(44);
+			vec5.push_back(43);
+			vec6.push_back(43);
+			vec7.push_back(42);
+			vec8.push_back(42);
+			vec9.push_back(43);
+			vec10.push_back(43);
+			vec11.push_back(43);
+			vec12.push_back(43);
+			vec13.push_back(44);
 		}
 	}
 }
