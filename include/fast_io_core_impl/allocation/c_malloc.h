96e56b5d0e603bd98d826807aafe6e4c1bac65de
trcrsired <uwgghhbcad@gmail.com>
2024-03-23 04:20:13 -0400

    aligned_alloc is useless. removed

--- include/fast_io_core_impl/allocation/c_malloc.h
+++ include/fast_io_core_impl/allocation/c_malloc.h
@@ -183,66 +183,6 @@ public:
 		}
 		::fast_io::noexcept_call(_aligned_free, p);
 	}
-#elif defined(_ISOC11_SOURCE)
-#if __has_cpp_attribute(__gnu__::__returns_nonnull__)
-	[[__gnu__::__returns_nonnull__]]
-#endif
-	static inline void *allocate_aligned(::std::size_t alignment, ::std::size_t n) noexcept
-	{
-		if (n == 0)
-		{
-			n = 1;
-		}
-		void *p = ::fast_io::noexcept_call(::aligned_alloc, alignment, n);
-		if (p == nullptr)
-		{
-			::fast_io::fast_terminate();
-		}
-		return p;
-	}
-#if __has_include(<malloc.h>) || __has_include(<malloc_np.h>)
-	static inline allocation_least_result allocate_aligned_at_least(::std::size_t alignment, ::std::size_t n) noexcept
-	{
-		auto p{::fast_io::c_malloc_allocator::allocate_aligned(alignment, n)};
-		return {p, ::fast_io::details::c_malloc_usable_size_impl(p)};
-	}
-	static inline allocation_least_result reallocate_aligned_at_least(void *oldp, ::std::size_t alignment, ::std::size_t n) noexcept
-	{
-		::std::size_t oldpsize{};
-		if (oldp)
-		{
-			oldpsize = ::fast_io::details::c_malloc_usable_size_impl(oldp);
-			if (n <= oldpsize)
-			{
-				return {oldp, oldpsize};
-			}
-		}
-		auto newresult{::fast_io::c_malloc_allocator::allocate_aligned_at_least(alignment, n)};
-		if (oldp != nullptr)
-		{
-			if (oldpsize) [[likely]]
-			{
-				__builtin_memcpy(newresult.ptr, oldp, oldpsize);
-			}
-#if defined(__has_builtin)
-#if __has_builtin(__builtin_free)
-			__builtin_free
-#else
-			::std::free
-#endif
-#else
-			::std::free
-#endif
-
-				(oldp);
-		}
-		return newresult;
-	}
-#endif
-	static inline void deallocate_aligned(void *p, ::std::size_t) noexcept
-	{
-		::fast_io::c_malloc_allocator::deallocate(p);
-	}
 #endif
 	static inline void deallocate(void *p) noexcept
 	{
