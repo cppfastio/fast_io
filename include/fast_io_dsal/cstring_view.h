d472190fb8570694fbd2bac66558c518ec9f7433
trcrsired <uwgghhbcad@gmail.com>
2024-04-12 21:55:54 -0400

    string_view.h

--- include/fast_io_dsal/cstring_view.h
+++ include/fast_io_dsal/cstring_view.h
@@ -24,30 +24,6 @@
 #endif
 
 #include "string_view.h"
-#include "impl/cstring_view.h"
-
-namespace fast_io
-{
-
-using ::fast_io::containers::null_terminated;
-using ::fast_io::containers::null_terminated_t;
-
-using cstring_view = ::fast_io::containers::basic_cstring_view<char>;
-using wcstring_view = ::fast_io::containers::basic_cstring_view<wchar_t>;
-using u8cstring_view = ::fast_io::containers::basic_cstring_view<char8_t>;
-using u16cstring_view = ::fast_io::containers::basic_cstring_view<char16_t>;
-using u32cstring_view = ::fast_io::containers::basic_cstring_view<char32_t>;
-
-namespace tlc
-{
-using cstring_view = ::fast_io::containers::basic_cstring_view<char>;
-using wcstring_view = ::fast_io::containers::basic_cstring_view<wchar_t>;
-using u8cstring_view = ::fast_io::containers::basic_cstring_view<char8_t>;
-using u16cstring_view = ::fast_io::containers::basic_cstring_view<char16_t>;
-using u32cstring_view = ::fast_io::containers::basic_cstring_view<char32_t>;
-} // namespace tlc
-
-} // namespace fast_io
 
 #if defined(_MSC_VER) && !defined(__clang__)
 #pragma warning(pop)
