bcf68dbd9e2f42bcee4c54607a214ddd51ea3061
trcrsired <uwgghhbcad@gmail.com>
2024-04-22 22:16:40 -0400

    Add transcoder

---
+++ include/fast_io_core_impl/concepts/transcoder.h
@@ -0,0 +1,52 @@
+#pragma once
+
+namespace fast_io
+{
+
+template <::std::integral fromchtype, ::std::integral tochtype>
+struct transcode_result
+{
+	using from_char_type = fromchtype;
+	using to_char_type = tochtype;
+	from_char_type const *from_ptr;
+	to_char_type *to_ptr;
+};
+
+template <typename T, typename fromchtype, typename tochtype>
+concept transcoder = ::std::integral<fromchtype> && ::std::integral<tochtype> &&
+					 requires(T t, fromchtype const *fromfirst, fromchtype const *fromlast,
+							  tochtype *tofirst, tochtype *tolast, ::std::size_t imsize) {
+						 { t.transcode_size(fromfirst, fromlast, imsize) } -> ::std::same_as<::std::size_t>;
+						 { t.transcode(fromfirst, fromlast, tofirst, tolast) } -> ::std::same_as<transcode_result<fromchtype, tochtype>>;
+					 };
+
+template <typename T, typename fromchtype, typename tochtype>
+concept eof_transcoder = transcoder<T, fromchtype, tochtype> &&
+						 requires(T t, fromchtype const *fromfirst, fromchtype const *fromlast,
+								  tochtype *tofirst, tochtype *tolast, ::std::size_t imsize) {
+							 { t.transcode_eof_size(fromfirst, fromlast, imsize) } -> ::std::same_as<::std::size_t>;
+							 { t.transcode_eof(fromfirst, fromlast, tofirst, tolast) } -> ::std::same_as<transcode_result<fromchtype, tochtype>>;
+						 };
+
+struct transcode_bytes_result
+{
+	::std::byte const *from_ptr;
+	::std::byte *to_ptr;
+};
+
+template <typename T>
+concept byte_transcoder = requires(T t, ::std::byte const *fromfirst, ::std::byte const *fromlast,
+								   ::std::byte *tofirst, ::std::byte *tolast, ::std::size_t imsize) {
+	{ t.transcode_bytes_size(fromfirst, fromlast, imsize) } -> ::std::same_as<::std::size_t>;
+	{ t.transcode_bytes(fromfirst, fromlast, tofirst, tolast) } -> ::std::same_as<transcode_bytes_result>;
+};
+
+template <typename T>
+concept byte_eof_transcoder = byte_transcoder<T> &&
+							  requires(T t, ::std::byte const *fromfirst, ::std::byte const *fromlast,
+									   ::std::byte *tofirst, ::std::byte *tolast, ::std::size_t imsize) {
+								  { t.transcode_bytes_eof_size(fromfirst, fromlast, imsize) } -> ::std::same_as<::std::size_t>;
+								  { t.transcode_bytes_eof(fromfirst, fromlast, tofirst, tolast) } -> ::std::same_as<transcode_bytes_result>;
+							  };
+
+} // namespace fast_io
