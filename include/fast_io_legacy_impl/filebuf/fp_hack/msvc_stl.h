45d3913a445a08ffcc05c31dd4daa4d4f1fa4552
trcrsired <uwgghhbcad@gmail.com>
2024-06-24 18:34:32 -0400

    Fix msvc_stl hack. need to check the dynamic_cast ptr to determine type before hacking

--- include/fast_io_legacy_impl/filebuf/fp_hack/msvc_stl.h
+++ include/fast_io_legacy_impl/filebuf/fp_hack/msvc_stl.h
@@ -81,7 +81,11 @@ inline FILE *fp_hack(T *fb) noexcept
 		if (fb) [[likely]]
 		{
 #ifdef __cpp_rtti
-			return ::fast_io::details::streambuf_hack::fp_hack_impl(dynamic_cast<::std::basic_filebuf<char_type, traits_type> *>(fb));
+			auto fbfb{dynamic_cast<::std::basic_filebuf<char_type, traits_type> *>(fb)};
+			if (fbfb)
+			{
+				return ::fast_io::details::streambuf_hack::fp_hack_impl(fbfb);
+			}
 #else
 			if constexpr (::std::same_as<char_type, char> || ::std::same_as<char_type, wchar_t> ||
 						  ::std::same_as<char_type, char8_t> || ::std::same_as<char_type, char16_t> ||
