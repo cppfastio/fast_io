5fcb5c976134b94bd7eeb0cbec3f44d395afb052
Gerodote <lg3dx6fd@gmail.com>
2024-07-18 12:14:11 +0000

    get back

--- include/fast_io_core_impl/utils.h
+++ include/fast_io_core_impl/utils.h
@@ -154,7 +154,7 @@ template <typename T>
 concept my_unsigned_integral = my_integral<T> && !my_signed_integral<T>;
 
 template <typename T>
-concept my_floating_point = ::std::is_floating_point<T>
+concept my_floating_point = ::std::floating_point<T>
 #ifdef __SIZEOF_FLOAT16__
 							|| ::std::same_as<::std::remove_cv_t<T>, __float16>
 #endif
