97f3dd3750e5fff2e4d9d573160234d317b64130
MacroModel <liyn_xlfd@163.com>
2024-08-03 22:43:48 +0800

    support msdosdjgpp filesystem

--- include/fast_io_hosted/filesystem/native.h
+++ include/fast_io_hosted/filesystem/native.h
@@ -3,13 +3,22 @@
 #include "apis.h"
 #include "fsutils.h"
 
-#if (!defined(__NEWLIB__) || defined(__CYGWIN__)) && !defined(_WIN32) && !defined(__MSDOS__) && \
-	__has_include(<dirent.h>) && !defined(_PICOLIBC__)
-#include "posix.h"
-#include "posix_at.h"
+#if (!defined(__NEWLIB__) || defined(__CYGWIN__)) && !defined(_WIN32) && !defined(__MSDOS__) && __has_include(<dirent.h>) && !defined(_PICOLIBC__)
+    #include "posix.h"
+    #include "posix_at.h"
 #endif
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-#include "nt.h"
-#include "nt_at.h"
+#if (defined(_WIN32) || defined(__CYGWIN__))
+    #if defined(_WIN32_WINDOWS)
+        #include "win9x.h"
+        #include "win9x_at.h"
+    #else
+        #include "nt.h"
+        #include "nt_at.h"
+    #endif
+#endif
+
+#if defined(__MSDOS__)
+    #include "dos.h"
+    #include "dos_at.h"
 #endif
