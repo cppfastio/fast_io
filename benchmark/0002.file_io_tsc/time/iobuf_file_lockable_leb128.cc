f6ec20794def3d7a9731787223cdb1b751008fd6
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-08-12 14:33:05 -0400

    leb128 lockable benchmark

--- benchmark/0002.file_io_tsc/time/iobuf_file_lockable_leb128.cc
+++ benchmark/0002.file_io_tsc/time/iobuf_file_lockable_leb128.cc
@@ -9,7 +9,7 @@ int main()
 	constexpr std::size_t N(10000000);
 	{
 		fast_io::timer t(u8"output");
-		fast_io::u8obuf_file obf(u8"iobuf_file_lockable_leb128.txt");
+		fast_io::u8obuf_file_lockable obf(u8"iobuf_file_lockable_leb128.txt");
 		for (std::size_t i{}; i != N; ++i)
 		{
 			print(obf, fast_io::mnp::leb128_put(i));
@@ -18,7 +18,7 @@ int main()
 	::fast_io::vector<std::size_t> vec(N);
 	{
 		fast_io::timer t(u8"input");
-		fast_io::u8ibuf_file ibf(u8"iobuf_file_lockable_leb128.txt");
+		fast_io::u8ibuf_file_lockable ibf(u8"iobuf_file_lockable_leb128.txt");
 		for (auto it{vec.begin()}; it != vec.cend(); ++it)
 		{
 			scan(ibf, fast_io::mnp::leb128_get(*it));
