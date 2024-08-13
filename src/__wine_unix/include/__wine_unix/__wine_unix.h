34bdde20d4b3aa0d48c269d0141af12937f7f077
trcrsired <uwgghhbcad@gmail.com>
2024-08-12 23:03:46 -0400

    wine_unix

--- src/__wine_unix/include/__wine_unix/__wine_unix.h
+++ src/__wine_unix/include/__wine_unix/__wine_unix.h
@@ -36,43 +36,31 @@ typedef __wine_host_fd_t __wine_host_mode_t;
 
 typedef struct
 {
-	__wine_errno_t errcode;
-}__wine_unix_result0_t;
-
-typedef struct
-{
-	__wine_errno_t errcode;
-	size_t value0;
-}__wine_unix_result1_t;
-
-typedef struct
-{
-	__wine_errno_t errcode;
-	size_t value0;
-	size_t value1;
-
-}__wine_unix_result2_t;
-
-typedef struct
-{
-	__wine_errno_t errcode;
-	size_t value0;
-	size_t value1;
-	size_t value2;
-
-}__wine_unix_result3_t;
-
-typedef struct
-{
 	void *iov_base;
 	size_t iov_len;
 }__wine_unix_iovec_t;
 
 __WINE_UNIX_DLLEXPORT __wine_errno_t __WINE_UNIX_DEFAULTCALL __wine_unix_host_errno_to_wine_errno(__wine_host_errno_t) __WINE_UNIX_NOEXCEPT;
 __WINE_UNIX_DLLEXPORT __wine_host_errno_t __WINE_UNIX_DEFAULTCALL __wine_unix_wine_errno_to_host_errno(__wine_errno_t) __WINE_UNIX_NOEXCEPT;
-__WINE_UNIX_DLLEXPORT __wine_unix_result3_t __WINE_UNIX_DEFAULTCALL __wine_unix_ret_writev(__wine_host_fd_t, __wine_unix_iovec_t const*, size_t) __WINE_UNIX_NOEXCEPT;
-__WINE_UNIX_DLLEXPORT __wine_unix_result3_t __WINE_UNIX_DEFAULTCALL __wine_unix_ret_readv(__wine_host_fd_t, __wine_unix_iovec_t*, size_t) __WINE_UNIX_NOEXCEPT;
-__WINE_UNIX_DLLEXPORT __wine_unix_result1_t __WINE_UNIX_DEFAULTCALL __wine_unix_ret_openat(__wine_host_fd_t, void const*, size_t, __wine_host_flags_t, __wine_host_mode_t) __WINE_UNIX_NOEXCEPT;
+
+struct
+{
+	__wine_errno_t errcode;
+	size_t total;
+	size_t baseindex;
+	size_t index;
+}__wine_unix_result_ret_readwritev;
+
+__WINE_UNIX_DLLEXPORT __wine_unix_result_ret_readwritev __WINE_UNIX_DEFAULTCALL __wine_unix_ret_writev(__wine_host_fd_t, __wine_unix_iovec_t const*, size_t) __WINE_UNIX_NOEXCEPT;
+
+__WINE_UNIX_DLLEXPORT __wine_unix_result_ret_readwritev __WINE_UNIX_DEFAULTCALL __wine_unix_ret_readv(__wine_host_fd_t, __wine_unix_iovec_t*, size_t) __WINE_UNIX_NOEXCEPT;
+
+struct
+{
+	__wine_errno_t errcode;
+	__wine_host_fd_t host_fd;
+}__wine_unix_result_ret_openat;
+__WINE_UNIX_DLLEXPORT __wine_unix_result_ret_openat __WINE_UNIX_DEFAULTCALL __wine_unix_ret_openat(__wine_host_fd_t, void const*, size_t, __wine_host_flags_t, __wine_host_mode_t) __WINE_UNIX_NOEXCEPT;
 __WINE_UNIX_DLLEXPORT __wine_errno_t __WINE_UNIX_DEFAULTCALL __wine_unix_ret_close(__wine_host_fd_t) __WINE_UNIX_NOEXCEPT;
 
 #ifdef __cplusplus
