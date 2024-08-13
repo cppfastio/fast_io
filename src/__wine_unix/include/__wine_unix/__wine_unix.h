eef23c11685cf044c497ce0912434997a2de9bf0
trcrsired <uwgghhbcad@gmail.com>
2024-08-12 23:08:58 -0400

    __wine_unix

--- src/__wine_unix/include/__wine_unix/__wine_unix.h
+++ src/__wine_unix/include/__wine_unix/__wine_unix.h
@@ -13,7 +13,7 @@
 #endif
 
 #ifdef __cplusplus
-#ifdef __cplusplus <= 201107
+#if __cplusplus <= 201107
 #define __WINE_UNIX_NOEXCEPT throw()
 #else
 #define __WINE_UNIX_NOEXCEPT noexcept
@@ -44,7 +44,7 @@ typedef struct
 __WINE_UNIX_DLLEXPORT __wine_errno_t __WINE_UNIX_DEFAULTCALL __wine_unix_host_errno_to_wine_errno(__wine_host_errno_t) __WINE_UNIX_NOEXCEPT;
 __WINE_UNIX_DLLEXPORT __wine_host_errno_t __WINE_UNIX_DEFAULTCALL __wine_unix_wine_errno_to_host_errno(__wine_errno_t) __WINE_UNIX_NOEXCEPT;
 
-struct
+typedef struct
 {
 	__wine_errno_t errcode;
 	size_t total;
@@ -56,11 +56,12 @@ __WINE_UNIX_DLLEXPORT __wine_unix_result_ret_readwritev __WINE_UNIX_DEFAULTCALL 
 
 __WINE_UNIX_DLLEXPORT __wine_unix_result_ret_readwritev __WINE_UNIX_DEFAULTCALL __wine_unix_ret_readv(__wine_host_fd_t, __wine_unix_iovec_t*, size_t) __WINE_UNIX_NOEXCEPT;
 
-struct
+typedef struct
 {
 	__wine_errno_t errcode;
 	__wine_host_fd_t host_fd;
 }__wine_unix_result_ret_openat;
+
 __WINE_UNIX_DLLEXPORT __wine_unix_result_ret_openat __WINE_UNIX_DEFAULTCALL __wine_unix_ret_openat(__wine_host_fd_t, void const*, size_t, __wine_host_flags_t, __wine_host_mode_t) __WINE_UNIX_NOEXCEPT;
 __WINE_UNIX_DLLEXPORT __wine_errno_t __WINE_UNIX_DEFAULTCALL __wine_unix_ret_close(__wine_host_fd_t) __WINE_UNIX_NOEXCEPT;
 
