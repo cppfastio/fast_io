bc5a002ea879ba2dc54bc3c72dd7725a50d2a6ce
trcrsired <uwgghhbcad@gmail.com>
2024-08-20 01:54:44 -0400

    test need ()?

--- tests/0024.timestamp/fixed.cc
+++ tests/0024.timestamp/fixed.cc
@@ -17,7 +17,7 @@ inline void test_val(::fast_io::unix_timestamp ts)
 		fixed(ts,::std::numeric_limits<::std::uint_least64_t>::digits10),"\n",
 		fixed(ts,::std::numeric_limits<::std::uint_least64_t>::digits10+1),"\n",
 		fixed(ts,::std::numeric_limits<::std::uint_least64_t>::digits10+2),"\n"
-		fixed(ts,::std::numeric_limits<::std::uint_least64_t>::digits10*2));
+		fixed(ts,(::std::numeric_limits<::std::uint_least64_t>::digits10)*2));
 }
 
 int main()
