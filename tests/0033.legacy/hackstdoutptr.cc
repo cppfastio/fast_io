6dcf6654996ae44913bbd425cac2680bac1eb647
trcrsired <uwgghhbcad@gmail.com>
2024-09-14 09:45:44 -0400

    add test for hack stdoutptr

--- tests/0033.legacy/hackstdoutptr.cc
+++ tests/0033.legacy/hackstdoutptr.cc
@@ -4,10 +4,11 @@ int main()
 {
 	using namespace fast_io::mnp;
 	using namespace fast_io::io;
-	fast_io::c_io_observer_unlocked ciob{stdout};
-	perrln(pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
-	println(40);
-	perrln(pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
-	println(40);
-	perrln(pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
+	::fast_io::c_io_observer_unlocked ciob{stdout};
+	for(::std::size_t i{};i!=100000u;++i)
+	{
+		perrln(ciob,pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
+		println(40);
+		perrln(ciob,pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
+	}
 }
