9c328630c6215fa31334111abcc6ff0e5d3c45cc
trcrsired <uwgghhbcad@gmail.com>
2024-09-14 09:43:27 -0400

    hackstdoutptr

---
+++ tests/0033.legacy/hackstdoutptr.cc
@@ -0,0 +1,13 @@
+﻿#include <fast_io.h>
+
+int main()
+{
+	using namespace fast_io::mnp;
+	using namespace fast_io::io;
+	fast_io::c_io_observer_unlocked ciob{stdout};
+	perrln(pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
+	println(40);
+	perrln(pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
+	println(40);
+	perrln(pointervw(obuffer_begin(ciob)), " ", pointervw(obuffer_curr(ciob)), " ", pointervw(obuffer_end(ciob)));
+}
