e604c42ff06db280916b8687531fa907b37023ca
trcrsired <uwgghhbcad@gmail.com>
2024-09-22 01:42:47 -0400

    string push back benchmark should be std one

--- benchmark/0011.containers/string/0001.string_push_back/std.cc
+++ benchmark/0011.containers/string/0001.string_push_back/std.cc
@@ -1,12 +1,12 @@
-﻿#include<fast_io_dsal/string.h>
+﻿#include<string>
 #include<fast_io_driver/timer.h>
 
 int main()
 {
-	::fast_io::u8string str;
+	::std::u8string str;
 	::std::size_t total_sum{};
 	{
-		::fast_io::timer t(u8"fast_io");
+		::fast_io::timer t(u8"std");
 		for(::std::size_t i{};i!=10000000u;++i)
 		{
 			str.push_back(u8'z');
