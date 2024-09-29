540248a5940379ddd7d895296a69c6ccffae8a09
havedifficultyinfindingnames <13918219745@163.com>
2024-09-29 15:15:24 +0800

    fixes

--- tests/0013.pr_rsv/bounds_checking_array.cc
+++ tests/0013.pr_rsv/bounds_checking_array.cc
@@ -8,5 +8,5 @@ int main()
 	std::array<char, 20> buffer;
 	// this would fail to compile
 	auto i{::fast_io::pr_rsv_to_array(buffer, fast_io::mnp::scientific(2.21421421412412214124124))};
-	println("buffer size:", buffer.size(), "\ndata:", fast_io::mnp::os_c_str_with_known_size(buffer.data(), i));
+	println("buffer size:", buffer.size(), "\ndata:", fast_io::mnp::os_c_str(buffer.data(), i));
 }
