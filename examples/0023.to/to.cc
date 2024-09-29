bb5c17d26528eb4957579d51cefb76a627406715
havedifficultyinfindingnames <13918219745@163.com>
2024-09-29 14:01:01 +0800

    fixes

--- examples/0023.to/to.cc
+++ examples/0023.to/to.cc
@@ -6,7 +6,7 @@ using namespace fast_io::io;
 int main()
 {
 #ifdef __SIZEOF_INT128__
-	constexpr __uint128_t s{fast_io::to<std::uint_least64_t>("   2424", fast_io::mnp::left(0, 30, '0'),
+	constexpr __uint128_t s{fast_io::to<__uint128_t>("   2424", fast_io::mnp::left(0, 30, '0'),
 													 " sgdjasdopgjdasgpojadsgpoadjsgpoadsjgopjaghdsoighasdioghadsoi")};
 	println(s);
 #endif
