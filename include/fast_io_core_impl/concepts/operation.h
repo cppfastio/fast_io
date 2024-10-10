c190d9946fc87849339660870d81c90332b850ca
havedifficultyinfindingnames <13918219745@163.com>
2024-10-10 17:14:19 +0800

    add comments for print and scan concepts

--- include/fast_io_core_impl/concepts/operation.h
+++ include/fast_io_core_impl/concepts/operation.h
@@ -3,6 +3,20 @@
 namespace fast_io
 {
 
+/// @brief    contiguous_scannable
+/// @details  That a type is contiguous scannable
+///           is that it can be scanned from a contiguous memory region
+///           passed in as a pointer to the beginning and end of the region
+///           as the 2nd and 3rd arguments in `scan_contiguous_define`.
+/// @fn       scan_contiguous_define
+/// @brief    Scans an object from a contiguous memory region.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, T>    tag-invoke
+/// @param    char_type const*                              a pointer to the beginning of the buffer
+/// @param    char_type const*                              a pointer to the end of the buffer
+/// @param    T                                             the object to be scanned, can be any passing style
+/// @return   ::fast_io::parse_result<char_type const*>     a pointer to the next after scanning,
+///                                                         and a parse code indicating parsing state
 template <typename char_type, typename T>
 concept contiguous_scannable = requires(char_type const *begin, char_type const *end, T t) {
 	{
@@ -10,6 +24,37 @@ concept contiguous_scannable = requires(char_type const *begin, char_type const 
 	} -> ::std::same_as<parse_result<char_type const *>>;
 };
 
+/// @brief    context_scannable
+/// @details  That a type is context scannable
+///           is that it can be scanned multiple times until it's fully scanned
+///           with the scanning state stored in a context object,
+///           each time from a contiguous memory region.
+/// @note     A type MUST be context scannable if it is to be scanned
+/// @fn       scan_context_type
+/// @brief    Indicates the type of your context object for context scannable types.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, T>    tag-invoke
+/// @return   ::fast_io::io_type_t<your_context_type>       the type of the context object
+/// @struct   your_context_type
+/// @brief    The context object, usually stores partial results.
+/// @fn       scan_context_define
+/// @breif    Partially scans an object from a contiguous memory region with a context object.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, T>    tag-invoke
+/// @param    your_context_type&                            the context object
+/// @param    char_type const*                              a pointer to the beginning of the buffer
+/// @param    char_type const*                              a pointer to the end of the buffer
+/// @param    T                                             the object to be scanned, can be any passing style
+/// @return   ::fast_io::parse_result<char_type const*>     a pointer to the next after scanning,
+///                                                         and a parse code indicating parsing state
+/// @note     It's not recommended to write to T if the scanning is still partial, so as to hold strong exception guarantee.
+/// @fn       scan_context_eof_define
+/// @breif    Indicates that the scanning meets an EOF.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, T>    tag-invoke
+/// @param    your_context_type&                            the context object
+/// @param    T                                             the object to be scanned, can be any passing style
+/// @return   ::fast_io::parse_code                         a parse code indicating parsing state
 template <typename char_type, typename T>
 concept context_scannable = requires(char_type const *begin, char_type const *end, T t) {
 	requires requires(
@@ -21,6 +66,21 @@ concept context_scannable = requires(char_type const *begin, char_type const *en
 	};
 };
 
+/// @brief    reserve_printable
+/// @details  That a type is reserve printable
+///           is that it can be printed to a buffer with a compile-time-known size.
+/// @fn       print_reserve_size
+/// @brief    Returns the size of the buffer needed to print an object at most.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @return   ::std::size_t                                 the size of the buffer to be reserved
+/// @fn       print_reserve_define
+/// @brief    Prints the object to a buffer.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param    char_type*                                    a pointer to the beginning of the buffer
+/// @param    T                                             the object to be printed
+/// @return   char_type*                                    a pointer to the next after printing
 template <typename char_type, typename T>
 concept reserve_printable = ::std::integral<char_type> && requires(T t, char_type *ptr) {
 	{
@@ -31,6 +91,23 @@ concept reserve_printable = ::std::integral<char_type> && requires(T t, char_typ
 	} -> ::std::convertible_to<char_type *>;
 };
 
+/// @brief    dynamic_reserve_printable
+/// @details  That a type is dynamic reserve printable
+///           is that it can be printed to a buffer with a run-time-known size,
+///           which can be different between objects of the same type.
+/// @fn       print_reserve_size
+/// @brief    Returns the size of the buffer needed to print an object at most.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param    T                                             the object to be printed
+/// @return   ::std::size_t                                 the size of the buffer to be reserved
+/// @fn       print_reserve_define
+/// @brief    Prints the object to a buffer.
+/// @tparam   <auto-inferred>
+/// @param    ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param    char_type*                                    a pointer to the beginning of the buffer
+/// @param    T                                             the object to be printed
+/// @return   char_type*                                    a pointer to the next after printing
 template <typename char_type, typename T>
 concept dynamic_reserve_printable = ::std::integral<char_type> && requires(T t, char_type *ptr) {
 	{
@@ -41,6 +118,26 @@ concept dynamic_reserve_printable = ::std::integral<char_type> && requires(T t, 
 	} -> ::std::convertible_to<char_type *>;
 };
 
+/// @warning    UNSTABLE
+/// @brief      context_printable
+/// @details    That a type is context printable
+///             is that it can be partially printed to a buffer multiple times.
+/// @fn         print_context_type
+/// @brief      Indicates the type of your context object for context printable types.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, T>  tag-invoke
+/// @return     ::fast_io::io_type_t<your_context_type>     the type of the context object
+/// @struct     your_context_type
+/// @brief      The context object, usually stores the progress of printing.
+/// @fn         print_context_define
+/// @breif      Partially prints an object to a buffer with a context object.
+/// @tparam     <auto-inferred>
+/// @param      this                                        the context object
+/// @param      T                                           the object to be printed
+/// @param      char_type*                                  a pointer to the beginning of the buffer
+/// @param      char_type*                                  a pointer to the end of the buffer
+/// @return     ::fast_io::context_print_result<char_type*> a pointer to the next after printing
+///                                                         and a boolean indicating whether the printing is done
 template <typename char_type, typename T>
 concept context_printable = ::std::integral<char_type> && requires(T t, char_type *ptr) {
 	requires requires(
@@ -49,6 +146,14 @@ concept context_printable = ::std::integral<char_type> && requires(T t, char_typ
 	};
 };
 
+/// @brief      printable_internal_shift
+/// @details    Defines the behaviour when printed with ::fast_io::mnp::width<::fast_io::mnp::scalar_placement::internal>
+/// @fn         print_define_internal_shift
+/// @brief      Returns the number of the characters printed on the left of the filling spaces.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param      T                                           the object to be printed
+/// @return     ::std::size_t                               the number of characters appaired on the left
 template <typename char_type, typename T>
 concept printable_internal_shift = requires(T t) {
 	{
@@ -56,6 +161,24 @@ concept printable_internal_shift = requires(T t) {
 	} -> ::std::same_as<::std::size_t>;
 };
 
+/// @brief      precise_reserve_printable
+/// @details    That a type is precise reserve printable
+///             is that it's `reserve_printable` or `dynamic_reserve_printable`
+///             and further has a precise printing size.
+/// @fn         print_reserve_precise_size
+/// @brief      Returns the precise size of the buffer needed to print an object.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param      T                                           the object to be printed
+/// @return     ::std::size_t                               the precise size of the buffer to be reserved
+/// @fn         print_reserve_precise_define
+/// @brief      Prints the object to a buffer with a precise size.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param      char_type*                                  a pointer to the beginning of the buffer
+/// @param      ::std::size_t                               the size of the buffer
+/// @param      T                                           the object to be printed
+/// @return     void
 template <typename char_type, typename T>
 concept precise_reserve_printable =
 	::std::integral<char_type> &&
@@ -67,6 +190,26 @@ concept precise_reserve_printable =
 		print_reserve_precise_define(io_reserve_type<char_type, ::std::remove_cvref_t<T>>, ptr, n, t);
 	};
 
+/// @brief      reserve_scatters_printable
+/// @details    That a type is reserve scatters printable
+///             is that it's composed of seperate parts
+///             that can be printed to several scatters with a compile-time-known size.
+/// @fn         print_reserve_scatters_size
+/// @brief      Returns the count of the scatters and some extra space required.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @return     ::fast_io::reserve_scatters_size_result     the count of the scatters and the size of an extra buffer
+///                                                         which can later be pointed in the scatters.
+/// @fn         print_reserve_scatters_define
+/// @brief      Prints the object to several scatters.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param      ::fast_io::basic_io_scatter_t<char_type>*   a pointer to the beginning of the scatters
+/// @param      char_type*                                  a pointer to the beginning of the buffer
+/// @param      T                                           the object to be printed
+/// @return     ::fast_io::basic_reserve_scatters_define_result<char_type>
+///                                                         a pointer to the next scatter after printing
+///                                                         and a pointer to the next character after printing
 template <typename char_type, typename T>
 concept reserve_scatters_printable =
 	::std::integral<char_type> && requires(T t, ::fast_io::basic_io_scatter_t<char_type> *scatters, char_type *ptr) {
@@ -78,11 +221,30 @@ concept reserve_scatters_printable =
 		} -> ::std::same_as<::fast_io::basic_reserve_scatters_define_result<char_type>>;
 	};
 
+/// @brief      printable
+/// @details    Makes a type printable
+/// @warning    This concept will be soon deprecated.
+/// @fn         print_define
+/// @brief      Prints the object to a device directly.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param      output                                      the output device
+/// @param      T                                           the object to be printed
+/// @return     void
 template <typename char_type, typename T>
 concept printable = requires(::fast_io::details::dummy_buffer_output_stream<char_type> out, T t) {
 	print_define(io_reserve_type<char_type, ::std::remove_cvref_t<T>>, out, t);
 };
 
+/// @brief      scatter_printable
+/// @details    That a type is scatter printable
+///             is that it can be printed to a scatter.
+/// @fn         print_scatter_define
+/// @brief      Generates a printed scatter.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_reserve_type_t<char_type, ::std::remove_cvref_t<T>> tag-invoke
+/// @param      T                                           the object to be printed
+/// @return     ::fast_io::basic_io_scatter_t<char_type>    a scatter of the printed object
 template <typename char_type, typename T>
 concept scatter_printable = requires(char_type ch, T &&t) {
 	{
@@ -91,9 +253,27 @@ concept scatter_printable = requires(char_type ch, T &&t) {
 	} -> ::std::same_as<basic_io_scatter_t<char_type>>;
 };
 
+/// @brief      alias_scannable
+/// @details    Alias scannable enables a type to be scanned using an alias
+///             where the scanning function is defined on the alias type.
+/// @fn         scan_alias_define
+/// @brief      Indicates the alias type.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_alias_t                       tag-invoke
+/// @param      T                                           the object to be scanned
+/// @return     your_alias_type                             your alias type, commonly owns a reference to T
 template <typename T>
 concept alias_scannable = requires(T &&t) { scan_alias_define(io_alias, t); };
 
+/// @brief      alias_printable
+/// @details    Alias printable enables a type to be printed using an alias
+///             where the printing function is defined on the alias type.
+/// @fn         print_alias_define
+/// @brief      Indicates the alias type.
+/// @tparam     <auto-inferred>
+/// @param      ::fast_io::io_alias_t                       tag-invoke
+/// @param      T                                           the object to be printed
+/// @return     your_alias_type                             your alias type, commonly owns T
 template <typename T>
 concept alias_printable = requires(T &&t) { print_alias_define(io_alias, ::fast_io::freestanding::forward<T>(t)); };
 
