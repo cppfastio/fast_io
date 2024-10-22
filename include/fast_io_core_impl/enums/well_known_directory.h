2bd055b0cad2dded90326db2d725ee683a2445b7
MacroModel <liyn_xlfd@163.com>
2024-10-22 18:50:26 +0800

    upd

---
+++ include/fast_io_core_impl/enums/well_known_directory.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+/*
+W3C standard
+File System Access
+Draft Community Group Report
+https://wicg.github.io/file-system-access/
+*/
+
+namespace fast_io
+{
+
+enum class well_known_directory : ::std::uint_least32_t
+{
+	desktop,
+	documents,
+	downloads,
+	music,
+	pictures,
+	videos
+};
+
+} // namespace fast_io
