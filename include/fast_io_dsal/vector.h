12b42f31d25494f9d73da59c7585d045b9b3de2a
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-11-05 04:34:53 -0500

    fix vector?

--- include/fast_io_dsal/vector.h
+++ include/fast_io_dsal/vector.h
@@ -55,7 +55,7 @@ vector(::std::from_range_t, R &&) -> vector<::std::ranges::range_value_t<R>, ::f
 
 template <typename T, typename... U>
 	requires(::std::constructible_from<T, U> && ...)
-vector(T, U...) -> vector<T, , ::fast_io::native_global_allocator>;
+vector(T, U...) -> vector<T, ::fast_io::native_global_allocator>;
 
 } // namespace containers
 
