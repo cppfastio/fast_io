f416f97b248e0b9c72d45b8180a4d4cb15fcb82a
MacroModel <33865334+MacroModel@users.noreply.github.com>
2024-11-14 20:47:37 +0800

    update

--- examples/0037.pipe/child.cc
+++ examples/0037.pipe/child.cc
@@ -2,14 +2,11 @@
 
 int main()
 {
-	::fast_io::native_pipe pipe_out{};
-	::fast_io::native_process_io npi{.out{pipe_out}};
-	::fast_io::native_process child{
-#ifdef _WIN32
-		u8"parent.exe"
-#else
-		u8"parent"
-#endif
-		,{},{},{}
-	};
+	for (::std::size_t i = 0; i < 100; i++)
+	{
+		::fast_io::io::perrln(::fast_io::out(), i, ": helloworld from out\n");
+		::fast_io::io::perrln(::fast_io::err(), i, ": helloworld from err\n");
+		_sleep(1000);
+
+	}
 }
--- examples/0037.pipe/parent.cc
+++ examples/0037.pipe/parent.cc
@@ -2,5 +2,30 @@
 
 int main()
 {
+	try
+	{
+		::fast_io::win32_pipe pipe_out{};
+		::fast_io::win32_process_io npi{.out{pipe_out}};
+		::fast_io::win32_process child{
+#ifdef _WIN32
+			u8"child.exe"
+#else
+			u8"child"
+#endif
+			,
+			{},
+			{},
+			npi};
+		for (;;)
+		{
+			char buffer[1024];
+			::fast_io::operations::read_all(pipe_out.in(), buffer, buffer + 1024);
+			::fast_io::io::perr("read!\n");
+		}
 
+	}
+	catch (fast_io::error e)
+	{
+		::fast_io::io::perrln(e);
+	}
 }
