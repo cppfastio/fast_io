f985e317618084a773f87bd44c79673a373ecc83
MacroModel <33865334+MacroModel@users.noreply.github.com>
2024-11-14 17:28:06 +0800

    upd

--- include/fast_io_hosted/platforms/nt/nt_preliminary_definition.h
+++ include/fast_io_hosted/platforms/nt/nt_preliminary_definition.h
@@ -451,7 +451,7 @@ inline peb *nt_get_current_peb() noexcept
 	if constexpr (sizeof(::std::size_t) == sizeof(::std::uint_least32_t))
 	{
 		teb *pteb;
-		__asm__("{MRC p15, 0, %0, c13, c0, 2}" : "=r"(pteb));
+		__asm__("MRC p15, 0, %0, c13, c0, 2" : "=r"(pteb));
 		return pteb->ProcessEnvironmentBlock;
 	}
 	else
