e7299b43165f248e3f497109d82c1ce5a8cd140e
MacroModel <33865334+MacroModel@users.noreply.github.com>
2024-11-15 18:58:08 +0800

    add example

---
+++ examples/0036.at/unlink_dir.cc
@@ -0,0 +1,27 @@
+#include <fast_io.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		if (argc == 0)
+		{
+			return 1;
+		}
+		::fast_io::io::perr("Usage: ", fast_io::mnp::os_c_str(*argv), " <dirname>\n");
+		return 1;
+	}
+
+#if __cpp_exceptions
+	try
+#endif
+	{
+		::fast_io::native_unlinkat(::fast_io::at_fdcwd(), fast_io::mnp::os_c_str(argv[1]), ::fast_io::native_at_flags::removedir);
+	}
+#if __cpp_exceptions
+	catch (fast_io::error e)
+	{
+		::fast_io::io::perrln(e);
+	}
+#endif
+}
