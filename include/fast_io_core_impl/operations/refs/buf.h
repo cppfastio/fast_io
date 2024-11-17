d9a1e70d85987ecf1b86fcfed39f1a7b2723a1d4
MacroModel <33865334+MacroModel@users.noreply.github.com>
2024-11-17 23:00:27 +0800

    upd

--- include/fast_io_core_impl/operations/refs/buf.h
+++ include/fast_io_core_impl/operations/refs/buf.h
@@ -5,17 +5,17 @@ namespace fast_io
 namespace operations::defines
 {
 template <typename handletype>
-concept available_add_ibuf = requires(handletype ht) {
+concept available_add_ibuf = requires {
 	typename decltype(::fast_io::operations::input_stream_ref(*static_cast<handletype *>(nullptr)))::input_char_type;
 };
 
 template <typename handletype>
-concept available_add_obuf = requires(handletype ht) {
+concept available_add_obuf = requires {
 	typename decltype(::fast_io::operations::output_stream_ref(*static_cast<handletype *>(nullptr)))::output_char_type;
 };
 
 template <typename handletype>
 concept available_add_iobuf = available_add_ibuf<handletype> || available_add_obuf<handletype>;
 
-}
+} // namespace operations::defines
 } // namespace fast_io
