26fd4b6886d7ed3dc78019b5262f0f2db4233f71
MacroModel <33865334+MacroModel@users.noreply.github.com>
2024-11-17 11:30:46 +0800

    fix utime

--- examples/0036.at/utime.cc
+++ examples/0036.at/utime.cc
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 		{
 			return 1;
 		}
-		::fast_io::io::perr("Usage: ", fast_io::mnp::os_c_str(*argv), " <file> [creation_time, now, omit] [last_modification_time, now, omit] [last_access_time, now, omit]\n");
+		::fast_io::io::perr("Usage: ", fast_io::mnp::os_c_str(*argv), " <file> [creation_time (iso8601), now, omit] [last_modification_time (iso8601), now, omit] [last_access_time (iso8601), now, omit]\n");
 		return 1;
 	}
 
@@ -22,9 +22,22 @@ int main(int argc, char **argv)
 		::fast_io::unix_timestamp_option last_access_time;
 		::fast_io::unix_timestamp_option last_modification_time;
 
-		if (::fast_io::scan<true>(fast_io::mnp::os_c_str(argv[2]), creation_time.timestamp))
+		::fast_io::iso8601_timestamp iso8601_ts{};
+
+		bool succussed{};
+		try
+		{
+			iso8601_ts = ::fast_io::to<::fast_io::iso8601_timestamp>(fast_io::mnp::os_c_str(argv[2]));
+			succussed = true;
+		}
+		catch (::fast_io::error e)
+		{
+			succussed = false;
+		}
+		if (succussed)
 		{
 			creation_time.flags = ::fast_io::utime_flags::none;
+			creation_time.timestamp = ::fast_io::to_timestamp(iso8601_ts);
 		}
 		else if (::fast_io::u8cstring_view{fast_io::mnp::os_c_str(reinterpret_cast<char8_t *>(argv[2]))} == u8"now")
 		{
@@ -40,9 +53,19 @@ int main(int argc, char **argv)
 			return 1;
 		}
 
-		if (::fast_io::scan<true>(fast_io::mnp::os_c_str(argv[3]), last_access_time.timestamp))
+		try
+		{
+			iso8601_ts = ::fast_io::to<::fast_io::iso8601_timestamp>(fast_io::mnp::os_c_str(argv[3]));
+			succussed = true;
+		}
+		catch (::fast_io::error e)
+		{
+			succussed = false;
+		}
+		if (succussed)
 		{
 			last_access_time.flags = ::fast_io::utime_flags::none;
+			last_access_time.timestamp = ::fast_io::to_timestamp(iso8601_ts);
 		}
 		else if (::fast_io::u8cstring_view{fast_io::mnp::os_c_str(reinterpret_cast<char8_t *>(argv[3]))} == u8"now")
 		{
@@ -58,9 +81,19 @@ int main(int argc, char **argv)
 			return 1;
 		}
 
-		if (::fast_io::scan<true>(fast_io::mnp::os_c_str(argv[4]), last_modification_time.timestamp))
+		try
+		{
+			iso8601_ts = ::fast_io::to<::fast_io::iso8601_timestamp>(fast_io::mnp::os_c_str(argv[4]));
+			succussed = true;
+		}
+		catch (::fast_io::error e)
+		{
+			succussed = false;
+		}
+		if (succussed)
 		{
 			last_modification_time.flags = ::fast_io::utime_flags::none;
+			last_modification_time.timestamp = ::fast_io::to_timestamp(iso8601_ts);
 		}
 		else if (::fast_io::u8cstring_view{fast_io::mnp::os_c_str(reinterpret_cast<char8_t *>(argv[4]))} == u8"now")
 		{
