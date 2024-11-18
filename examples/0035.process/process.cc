1919cc849f0a24cc954834bc3c3ad757ab2c69b6
trcrsired <uwgghhbcad@gmail.com>
2024-11-18 01:23:11 -0500

    where is the output?

--- examples/0035.process/process.cc
+++ examples/0035.process/process.cc
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 		::fast_io::io::perr("Usage: ", ::fast_io::mnp::os_c_str(*argv), " <exe>\n");
 		return 1;
 	}
-	::fast_io::native_process p{::fast_io::mnp::os_c_str(argv[1]), {}, {}, {}};
+	::fast_io::native_process p{::fast_io::mnp::os_c_str(argv[1]), {}, {}, {.in = fast_io::in(), .out = fast_io::out(), .err = fast_io::err()}};
 	auto ec{wait(p)};
 	::fast_io::io::perrln(::fast_io::mnp::os_c_str(*argv), " -> Exit code: ", static_cast<int>(ec.wait_loc));
 }
