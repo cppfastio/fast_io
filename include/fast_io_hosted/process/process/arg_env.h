cefa16a78ea9dfbe8e16b567c5b77000024c4619
MacroModel <33865334+MacroModel@users.noreply.github.com>
2024-11-26 17:43:23 +0800

    revamp posix_process_args and support append

--- include/fast_io_hosted/process/process/arg_env.h
+++ include/fast_io_hosted/process/process/arg_env.h
@@ -165,6 +165,11 @@ struct win32_process_args
 			return reinterpret_cast<char_type_may_alias_const_ptr>(args.c_str());
 		}
 	}
+
+	inline constexpr void append(win32_process_args const &others) noexcept
+	{
+		args.append(others.args);
+	}
 };
 
 template <::fast_io::win32_family family>
@@ -219,6 +224,11 @@ struct win32_process_envs
 			return reinterpret_cast<char_type_may_alias_const_ptr>(envs.c_str());
 		}
 	}
+
+	inline constexpr void append(win32_process_envs const &others) noexcept
+	{
+		envs.append(others.envs);
+	}
 };
 
 using nt_process_args = ::fast_io::win32_process_args<::fast_io::win32_family::wide_nt>;
@@ -229,30 +239,114 @@ using nt_process_envs = ::fast_io::win32_process_envs<::fast_io::win32_family::w
 // posix
 namespace details
 {
+
+template <::std::integral char_type>
+struct cstr_guard
+{
+	using Alloc = ::fast_io::native_typed_global_allocator<char_type>;
+
+	char_type *cstr;
+
+	inline constexpr cstr_guard() noexcept = default;
+
+	inline constexpr cstr_guard(cstr_guard const &others) noexcept
+	{
+		::std::size_t str_size{::fast_io::cstr_len(others.cstr)};
+		cstr = Alloc::allocate(str_size + 1);
+		auto const lase_ptr{::fast_io::freestanding::non_overlapped_copy_n(others.cstr, str_size, cstr)};
+		*lase_ptr = ::fast_io::char_literal_v<u8'\0', char_type>;
+	}
+
+	inline constexpr cstr_guard &operator=(cstr_guard const &others) noexcept
+	{
+		if (__builtin_addressof(others) == this) [[unlikely]]
+		{
+			return *this;
+		}
+
+		if (cstr)
+		{
+			Alloc::deallocate(cstr);
+		}
+
+		::std::size_t str_size{::fast_io::cstr_len(others.cstr)};
+		cstr = Alloc::allocate(str_size + 1);
+		auto const lase_ptr{::fast_io::freestanding::non_overlapped_copy_n(others.cstr, str_size, cstr)};
+		*lase_ptr = ::fast_io::char_literal_v<u8'\0', char_type>;
+	}
+
+	inline constexpr cstr_guard(cstr_guard &&others) noexcept
+	{
+		cstr = others.cstr;
+		others.cstr = nullptr;
+	}
+
+	inline constexpr cstr_guard &operator=(cstr_guard &&others) noexcept
+	{
+		if (__builtin_addressof(others) == this) [[unlikely]]
+		{
+			return *this;
+		}
+
+		if (cstr)
+		{
+			Alloc::deallocate(cstr);
+		}
+		cstr = others.cstr;
+		others.cstr = nullptr;
+	}
+
+	inline constexpr ~cstr_guard()
+	{
+		if (cstr)
+		{
+			Alloc::deallocate(cstr);
+			cstr = nullptr;
+		}
+	}
+};
+} // namespace details
+
+namespace freestanding
+{
+template <::std::integral char_type>
+struct is_trivially_relocatable<details::cstr_guard<char_type>>
+{
+	inline static constexpr bool value = true;
+};
+
+template <::std::integral char_type>
+struct is_zero_default_constructible<details::cstr_guard<char_type>>
+{
+	inline static constexpr bool value = true;
+};
+
+} // namespace freestanding
+
+namespace details
+{
 template <::std::size_t N, ::std::integral replace_char_type, typename T>
 inline constexpr void construct_posix_process_argenvs_decay_singal(
-	::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator> &str, T t)
+	::fast_io::containers::vector<details::cstr_guard<replace_char_type>, ::fast_io::native_global_allocator> &str, T t)
 {
 	constexpr bool type_error{::fast_io::operations::defines::print_freestanding_okay<::fast_io::details::dummy_buffer_output_stream<replace_char_type>, T>};
 
-	using ptrdiff_t_may_alias_ptr
-#if __has_cpp_attribute(__gnu__::__may_alias__)
-		[[__gnu__::__may_alias__]]
-#endif
-		= ::std::ptrdiff_t *;
-
-	auto sz{static_cast<::std::ptrdiff_t>(str.size())};
-	::fast_io::freestanding::my_memcpy(reinterpret_cast<ptrdiff_t_may_alias_ptr>(str.begin()) + N, __builtin_addressof(sz), sizeof(sz));
+	::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator> cstr;
+	details::cstr_guard<replace_char_type> cstrg;
 
 	if constexpr (type_error)
 	{
-		::fast_io::io_strlike_reference_wrapper<replace_char_type, ::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator>> wrapper{__builtin_addressof(str)};
-		::fast_io::operations::print_freestanding<false>(wrapper, t, ::fast_io::mnp::chvw(::fast_io::char_literal_v<u8'\0', replace_char_type>));
+		auto cstr{::fast_io::basic_general_concat<false, replace_char_type, ::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator>>(t)};
+		cstrg.cstr = cstr.imp.begin_ptr;
+		cstr.imp = {};
+		str.push_back(::std::move(cstrg));
 	}
 	else if constexpr (requires { ::fast_io::mnp::code_cvt(t); })
 	{
-		::fast_io::io_strlike_reference_wrapper<replace_char_type, ::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator>> wrapper{__builtin_addressof(str)};
-		::fast_io::operations::print_freestanding<false>(wrapper, ::fast_io::mnp::code_cvt(t), ::fast_io::mnp::chvw(::fast_io::char_literal_v<u8'\0', replace_char_type>));
+		auto cstr{::fast_io::basic_general_concat<false, replace_char_type, ::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator>>(::fast_io::mnp::code_cvt(t))};
+		cstrg.cstr = cstr.imp.begin_ptr;
+		cstr.imp = {};
+		str.push_back(::std::move(cstrg));
 	}
 	else
 	{
@@ -262,7 +356,7 @@ inline constexpr void construct_posix_process_argenvs_decay_singal(
 
 template <::std::size_t N = 0, ::std::integral replace_char_type, typename T, typename... Args>
 inline constexpr void construct_posix_process_argenvs_decay(
-	::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator> &str, T t, Args... args)
+	::fast_io::containers::vector<details::cstr_guard<replace_char_type>, ::fast_io::native_global_allocator> &str, T t, Args... args)
 {
 	construct_posix_process_argenvs_decay_singal<N>(str, t);
 
@@ -277,7 +371,7 @@ struct posix_process_args
 {
 	using char_type = char;
 	using replace_char_type = char;
-	using storage_type = ::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator>;
+	using storage_type = ::fast_io::containers::vector<details::cstr_guard<replace_char_type>, ::fast_io::native_global_allocator>;
 	using char_type_may_alias_ptr
 #if __has_cpp_attribute(__gnu__::__may_alias__)
 		[[__gnu__::__may_alias__]]
@@ -291,59 +385,55 @@ struct posix_process_args
 
 	storage_type arg_envs{};
 
-	inline constexpr posix_process_args() noexcept = default;
+	inline constexpr posix_process_args() noexcept
+	{
+		arg_envs.emplace_back(); // nullptr
+	}
 
 	inline constexpr posix_process_args(default_args_t, char const *const *args) noexcept
 	{
-		arg_envs = storage_type{sizeof(args) / sizeof(char_type)};
-		::fast_io::freestanding::my_memcpy(arg_envs.begin(), __builtin_addressof(args), sizeof(args));
+		for (char const *const *curr{args}; *curr; ++curr)
+		{
+			details::cstr_guard<replace_char_type> str;
+			::std::size_t str_size{::fast_io::cstr_len(*curr)};
+			str.cstr = details::cstr_guard<replace_char_type>::Alloc::allocate(str_size + 1);
+			auto const lase_ptr{::fast_io::freestanding::non_overlapped_copy_n(*curr, str_size, str.cstr)};
+			*lase_ptr = ::fast_io::char_literal_v<u8'\0', replace_char_type>;
+			arg_envs.push_back(::std::move(str));
+		}
+		arg_envs.emplace_back(); // nullptr
 	}
 
 	template <typename T, typename... Args>
 		requires(!::std::same_as<::std::remove_cvref_t<T>, default_args_t>)
 	inline constexpr posix_process_args(T &&t, Args &&...as)
 	{
-		auto sizeofptr{sizeof...(Args) + 2}; // T, Args..., nullptr
-		arg_envs = ::fast_io::containers::basic_string<replace_char_type, ::fast_io::native_global_allocator>{sizeofptr * sizeof(::std::ptrdiff_t) / sizeof(replace_char_type)}; // calloc
-
 		details::construct_posix_process_argenvs_decay(arg_envs,
 													   ::fast_io::io_print_forward<replace_char_type>(::fast_io::io_print_alias(t)),
 													   ::fast_io::io_print_forward<replace_char_type>(::fast_io::io_print_alias(as))...);
-		
-		using ptrdiff_t_may_alias_ptr
-#if __has_cpp_attribute(__gnu__::__may_alias__)
-			[[__gnu__::__may_alias__]]
-#endif
-			= ::std::ptrdiff_t *;
-
-		auto const begin{reinterpret_cast<ptrdiff_t_may_alias_ptr>(arg_envs.begin())};
-		auto const end{begin + sizeof...(Args) + 1};
-
-		for (auto curr{begin}; curr != end; ++curr)
-		{
-			::std::ptrdiff_t sz{};
-			::fast_io::freestanding::my_memcpy(__builtin_addressof(sz), curr, sizeof(sz));
-			auto const ptr{arg_envs.cbegin() + sz};
-			::fast_io::freestanding::my_memcpy(curr, __builtin_addressof(ptr), sizeof(ptr));
-		}
-
+		arg_envs.emplace_back(); // nullptr
 	}
 
 	inline constexpr char const *const *get() const noexcept
 	{
-		if (auto const szb{arg_envs.size_bytes()}; szb == 0)
+		return (char const *const *)(arg_envs.data());
+	}
+
+	inline constexpr void append(posix_process_args const &others) noexcept
+	{
+		if (others.arg_envs.size() > 1) [[likely]]
 		{
-			return nullptr;
-		}
-		else if (szb == sizeof(::std::ptrdiff_t))
-		{
-			char const *const *ptr;
-			::fast_io::freestanding::my_memcpy(__builtin_addressof(ptr), arg_envs.cbegin(), sizeof(ptr));
-			return ptr;
-		}
-		else
-		{
-			return (char const *const *)(arg_envs.c_str());
+			arg_envs.pop_back(); // check and rm nullptr
+
+			arg_envs.reserve(arg_envs.size() + others.arg_envs.size()); // sz + o.sz + 1, no need to check
+
+			auto const end{others.arg_envs.cend() - 1}; // without nullptr
+			for (auto curr{others.arg_envs.cbegin()}; curr != end; ++curr)
+			{
+				arg_envs.emplace_back_unchecked(*curr);
+			}
+
+			arg_envs.emplace_back_unchecked(); // nullptr
 		}
 	}
 };
