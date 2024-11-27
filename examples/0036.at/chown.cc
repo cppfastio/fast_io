6cf7802b6429ad9e76c9abc3646718f64cd94eaf
MacroModel <33865334+MacroModel@users.noreply.github.com>
2024-11-27 21:36:06 +0800

    update chown example

--- examples/0036.at/chown.cc
+++ examples/0036.at/chown.cc
@@ -20,8 +20,16 @@ int main(int argc, char **argv)
 		::std::uint_least64_t o;
 		::std::uint_least64_t g;
 
-		::fast_io::ibuffer_view ibv{argv[2], argv[2] + ::fast_io::cstr_len(argv[2])};
-		::fast_io::scan(ibv, ::fast_io::mnp::hex_get<8>(o), ::fast_io::mnp::hex_get<8>(g));
+		auto const [p1,e1]= ::fast_io::parse_by_scan(argv[2], argv[2] + ::fast_io::cstr_len(argv[2]), ::fast_io::mnp::hex_get<8>(o));
+		if (e1 != ::fast_io::parse_code::ok)
+		{
+			::fast_io::throw_parse_code(e1);
+		}
+		auto const [p2, e2] = ::fast_io::parse_by_scan(argv[3], argv[3] + ::fast_io::cstr_len(argv[3]), ::fast_io::mnp::hex_get<8>(g));
+		if (e2 != ::fast_io::parse_code::ok)
+		{
+			::fast_io::throw_parse_code(e2);
+		}
 		::fast_io::native_fchownat(::fast_io::at_fdcwd(), fast_io::mnp::os_c_str(argv[1]), o, g);
 	}
 #if __cpp_exceptions
