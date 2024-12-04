d02d1910d95ceaa34dd39e59ecba45e4b1d8333d
MacroModel <MacroModel_Traject@outlook.com>
2024-12-04 16:23:31 +0800

    rename

--- include/fast_io_legacy_impl/c/impl.h
+++ include/fast_io_legacy_impl/c/impl.h
@@ -835,10 +835,10 @@ inline constexpr posix_file_status status(basic_c_family_io_observer<family, ch_
 	return status(static_cast<basic_posix_io_observer<ch_type>>(ciob));
 }
 
-template <c_family family, ::std::integral char_type>
-inline ::std::size_t file_size(::fast_io::basic_c_family_io_observer<family, char_type> observer)
+template <c_family family, ::std::integral ch_type>
+inline ::std::size_t file_size(::fast_io::basic_c_family_io_observer<family, ch_type> ciob)
 {
-	return ::fast_io::details::posix_loader_get_file_size(details::my_fileno_impl<family>(observer.fp));
+	return ::fast_io::details::posix_loader_get_file_size(details::my_fileno_impl<family>(ciob.fp));
 }
 #endif
 
