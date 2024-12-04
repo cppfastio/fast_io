101a98bd6a87c84365200d7109f9ad5351523354
trcrsired <uwgghhbcad@gmail.com>
2024-12-04 14:00:14 -0500

    fuzzing

--- fuzzing/0000.write/c_file_unlocked.cc
+++ fuzzing/0000.write/c_file_unlocked.cc
@@ -2,8 +2,9 @@
 
 fast_io::c_file_unlocked obf("/dev/null", fast_io::open_mode::out);
 
-extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *ptr, std::size_t n) noexcept
+extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *pptr, std::size_t n) noexcept
 {
-	write(obf, ptr, ptr + n);
+	::std::byte const* ptr{reinterpret_cast<::std::byte const*>(pptr)};
+	::fast_io::operations::write_all_bytes(obf, ptr, ptr + n);
 	return 0;
 }
