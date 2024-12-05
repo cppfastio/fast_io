8313ca5d234ed0abba73e3429adce48e20213b2b
trcrsired <uwgghhbcad@gmail.com>
2024-12-05 16:05:28 -0500

    add wfilebuf file

---
+++ fuzzing/0000.write/filebuf_file.cc
@@ -0,0 +1,21 @@
+﻿#include <fast_io.h>
+#include <fast_io_legacy.h>
+
+fast_io::filebuf_file obf(
+#if defined(_WIN32) && !defined(__WINE__)
+	"nul"
+#else
+	"/dev/null"
+#endif
+	,
+	fast_io::open_mode::out);
+
+
+extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *pptr, std::size_t n) noexcept
+{
+	char const *ptr{reinterpret_cast<char const *>(pptr)};
+	using namespace fast_io::mnp;
+	::fast_io::operations::write_all(obf, ptr, ptr + n);
+
+	return 0;
+}
---
+++ fuzzing/0000.write/wfilebuf_file.cc
@@ -0,0 +1,21 @@
+﻿#include <fast_io.h>
+#include <fast_io_legacy.h>
+
+fast_io::wfilebuf_file obf(
+#if defined(_WIN32) && !defined(__WINE__)
+	"nul"
+#else
+	"/dev/null"
+#endif
+	,
+	fast_io::open_mode::out);
+
+
+extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *pptr, std::size_t n) noexcept
+{
+	wchar_t const *ptr{reinterpret_cast<wchar_t const *>(pptr)};
+	using namespace fast_io::mnp;
+	::fast_io::operations::write_all(obf, ptr, ptr + n / sizeof(wchar_t));
+
+	return 0;
+}
