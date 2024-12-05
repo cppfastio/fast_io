e86476167584c8236c4795c993498c5471b3ccd3
trcrsired <uwgghhbcad@gmail.com>
2024-12-05 15:06:45 -0500

    add more fuzz

--- fuzzing/0000.write/c_file_unlocked.cc
+++ fuzzing/0000.write/c_file_unlocked.cc
@@ -1,10 +1,17 @@
 ﻿#include <fast_io.h>
 
-fast_io::c_file_unlocked obf("nul", fast_io::open_mode::out);
+fast_io::c_file_unlocked obf(
+#if defined(_WIN32) && !defined(__WINE__)
+	"nul"
+#else
+	"/dev/null"
+#endif
+	,
+	fast_io::open_mode::out);
 
 extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *pptr, std::size_t n) noexcept
 {
-	::std::byte const* ptr{reinterpret_cast<::std::byte const*>(pptr)};
+	::std::byte const *ptr{reinterpret_cast<::std::byte const *>(pptr)};
 	::fast_io::operations::write_all_bytes(obf, ptr, ptr + n);
 	return 0;
 }
--- fuzzing/0000.write/wc_file_unlocked.cc
+++ fuzzing/0000.write/wc_file_unlocked.cc
@@ -6,15 +6,16 @@ fast_io::wc_file_unlocked obf(
 #else
 	"/dev/null"
 #endif
-	, fast_io::open_mode::out);
+	,
+	fast_io::open_mode::out);
 
 extern "C"
-[[__gnu__::__weak__]]
-uintptr_t __sancov_lowest_stack{};
+	[[__gnu__::__weak__]]
+	uintptr_t __sancov_lowest_stack{};
 
 extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *pptr, std::size_t n) noexcept
 {
-	wchar_t const* ptr{reinterpret_cast<wchar_t const*>(pptr)};
-	::fast_io::operations::write_all(obf, ptr, ptr + n/sizeof(wchar_t));
+	wchar_t const *ptr{reinterpret_cast<wchar_t const *>(pptr)};
+	::fast_io::operations::write_all(obf, ptr, ptr + n / sizeof(wchar_t));
 	return 0;
 }
