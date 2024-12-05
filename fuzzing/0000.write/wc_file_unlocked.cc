63d5ceb40ba8ff27728558c8469b350e354ea7ab
trcrsired <uwgghhbcad@gmail.com>
2024-12-05 15:32:29 -0500

    add fusewide

--- fuzzing/0000.write/wc_file_unlocked.cc
+++ fuzzing/0000.write/wc_file_unlocked.cc
@@ -13,9 +13,22 @@ extern "C"
 	[[__gnu__::__weak__]]
 	uintptr_t __sancov_lowest_stack{};
 
+struct usefwide
+{
+	usefwide()
+	{
+		fwide(obf.fp, 1);
+	}
+};
+#ifdef USE_FWIDE
+usefwide t;
+#endif
+
 extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *pptr, std::size_t n) noexcept
 {
 	wchar_t const *ptr{reinterpret_cast<wchar_t const *>(pptr)};
+	using namespace fast_io::mnp;
 	::fast_io::operations::write_all(obf, ptr, ptr + n / sizeof(wchar_t));
+
 	return 0;
 }
