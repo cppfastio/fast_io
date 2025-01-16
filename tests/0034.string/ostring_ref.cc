8711d7bf838a4685c7b18e29227f36b5872f90e4
trcrsired <uwgghhbcad@gmail.com>
2025-01-16 07:39:30 -0500

    add test for ostring_ref

--- tests/0034.string/ostring_ref.cc
+++ tests/0034.string/ostring_ref.cc
@@ -1,12 +1,14 @@
+#include <string>
 #include <fast_io.h>
 #include <fast_io_dsal/string.h>
+#include <fast_io_dsal/string_view.h>
 
 int main()
 {
 	using namespace fast_io::io;
+	::std::string output1;
 	{
-	::std::string output;
-	::fast_io::ostring_ref_std obf{__builtin_addressof(output)};
+	::fast_io::ostring_ref_std obf{__builtin_addressof(output1)};
 	print(obf, "CREATE TABLE t1(a INTEGER, b INTEGER, c VARCHAR(100));\n");
 	for (::std::size_t i{1}; i <= 1000; ++i)
 	{
@@ -14,11 +16,10 @@ int main()
 		print(obf, "INSERT INTO t1 VALUES(", i, ",", val, ",\'", val,
 			  "\');\n");
 	}
-	println(output);
 	}
+	::fast_io::string output2;
 	{
-	::fast_io::string output;
-	::fast_io::ostring_ref_fast_io obf{__builtin_addressof(output)};
+	::fast_io::ostring_ref_fast_io obf{__builtin_addressof(output2)};
 	print(obf, "CREATE TABLE t1(a INTEGER, b INTEGER, c VARCHAR(100));\n");
 	for (::std::size_t i{1}; i <= 1000; ++i)
 	{
@@ -26,6 +27,9 @@ int main()
 		print(obf, "INSERT INTO t1 VALUES(", i, ",", val, ",\'", val,
 			  "\');\n");
 	}
-	println(output);
 	}
+	println(output1,output2,
+		"output1<=>output2:",
+		::fast_io::string_view(::fast_io::freestanding::from_range, output1)<=>
+		::fast_io::string_view(::fast_io::freestanding::from_range, output2));
 }
