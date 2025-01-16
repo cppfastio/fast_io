46e67021478af7189ef50191c6ea6bff6dfa1d45
trcrsired <uwgghhbcad@gmail.com>
2025-01-16 06:39:38 -0500

    revert the change since it is pointless

--- include/fast_io_core_impl/operations/writeimpl/basis.h
+++ include/fast_io_core_impl/operations/writeimpl/basis.h
@@ -517,9 +517,7 @@ inline constexpr void write_all_impl(outstmtype outsm, typename outstmtype::outp
 			[[likely]]
 #endif
 		{
-			auto newcurr{curr + itdiff};
-			obuffer_set_curr(outsm, newcurr);
-			non_overlapped_copy(first, last, curr);
+			obuffer_set_curr(outsm, non_overlapped_copy_n(first, static_cast<::std::size_t>(itdiff), curr));
 			return;
 		}
 	}
