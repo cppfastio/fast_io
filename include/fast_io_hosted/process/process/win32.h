536dcc85d5276798cc980d386ce397a94fffefa8
trcrsired <uwgghhbcad@gmail.com>
2025-01-22 18:34:41 -0500

    detect self move

--- include/fast_io_hosted/process/process/win32.h
+++ include/fast_io_hosted/process/process/win32.h
@@ -789,6 +789,10 @@ public:
 	}
 	inline win32_family_process &operator=(win32_family_process &&__restrict b) noexcept
 	{
+		if (__builtin_addressof(b) == this)
+		{
+			return *this;
+		}
 		win32::details::close_win32_user_process_information_and_wait(this->hnt_user_process_info);
 		this->hnt_user_process_info = b.release();
 		return *this;
