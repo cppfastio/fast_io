d9cefcbec1de8cf900988d41db11ce0f6af63e49
MacroModel <MacroModel_Traject@outlook.com>
2025-01-28 13:42:08 +0800

    update process example

--- examples/0035.process/args_envs.cc
+++ examples/0035.process/args_envs.cc
@@ -1,5 +1,5 @@
 ﻿#include <fast_io.h>
-
+#include <fast_io_dsal/string.h>
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -11,6 +11,12 @@ int main(int argc, char **argv)
 		::fast_io::io::perr("Usage: ", ::fast_io::mnp::os_c_str(*argv), " <exe>\n");
 		return 1;
 	}
+
+#ifdef _WIN32
+	// ::fast_io::default_args : Same parameters as the corresponding system API
+	[[maybe_unused]] ::fast_io::native_process_args native_args{::fast_io::default_args, u""};
+#endif
+
 	::fast_io::native_process p{
 		::fast_io::mnp::os_c_str(argv[1]),
 		{"char",
@@ -19,9 +25,14 @@ int main(int argc, char **argv)
 		 u"u16",
 		 U"u32",
 		 114514,
-		 115145.1919810},
-		{},
-		{}};
+		 115145.1919810,
+		 ::fast_io::concat_fast_io("This ", 1, "s ", ::fast_io::mnp::code_cvt_os_c_str(u8"just "), "a parameter")},
+		{"env1",
+		 L"env2",
+		 3,
+		 4.0},
+		{.in = ::fast_io::posix_dev_null(), .out = ::fast_io::out(), .err = ::fast_io::err()},
+		::fast_io::process_mode::none};
 	auto ec{wait(p)};
 	::fast_io::io::perrln(::fast_io::mnp::os_c_str(*argv), " -> Exit code: ", static_cast<int>(ec.wait_loc));
 }
