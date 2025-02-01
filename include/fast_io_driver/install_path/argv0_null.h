f8670fd0c25efabf4dc87aa5542b4a203500d3c1
MacroModel <MacroModel_Traject@outlook.com>
2025-02-02 02:10:11 +0800

    rm const attribute in install path driver

--- include/fast_io_driver/install_path/argv0_null.h
+++ include/fast_io_driver/install_path/argv0_null.h
@@ -5,9 +5,6 @@ namespace fast_io::details
 #if __has_cpp_attribute(__gnu__::__cold__)
 [[__gnu__::__cold__]]
 #endif
-#if __has_cpp_attribute(__gnu__::__const__)
-[[__gnu__::__const__]]
-#endif
 [[noreturn]]
 inline ::fast_io::install_path get_module_install_path_from_argv0(char const *argv0) 
 {
