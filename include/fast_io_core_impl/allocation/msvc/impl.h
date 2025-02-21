e31e5afbaef0e0c21c8fdc961c57c129dcb51186
trcrsired <uwgghhbcad@gmail.com>
2025-02-21 06:21:43 -0500

    add errors for arm64ec

--- include/fast_io_core_impl/allocation/msvc/impl.h
+++ include/fast_io_core_impl/allocation/msvc/impl.h
@@ -3,6 +3,7 @@
 #if SIZE_MAX > UINT_LEAST32_MAX
 #include "msvc_linker_64.h"
 #if defined(__arm64ec__) || defined(_M_ARM64EC)
+#error "We do not support ARM64EC. See reasons: https://github.com/trcrsired/Why-Microsoft-ARM64EC-ABI-MUST-DIE/blob/main/README.md"
 #include "msvc_linker_arm64ec.h"
 #endif
 #elif defined(__x86__) || defined(_M_IX86) || defined(__i386__)
