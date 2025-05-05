9eca55ad890cc8294154f11518dc970144d6e2d6
MacroModel <33865334+MacroModel@users.noreply.github.com>
2025-05-06 02:26:25 +0800

    fix nt_api.h (#1146)

--- include/fast_io_hosted/platforms/nt/nt_api.h
+++ include/fast_io_hosted/platforms/nt/nt_api.h
@@ -138,6 +138,6 @@ FAST_IO_DLLIMPORT ::std::uint_least32_t FAST_IO_WINSTDCALL RtlAcquirePrivilege(:
 FAST_IO_DLLIMPORT void FAST_IO_WINSTDCALL RtlReleasePrivilege(void *) noexcept FAST_IO_WINSTDCALL_RENAME(RtlReleasePrivilege, 4);
 FAST_IO_DLLIMPORT ::std::uint_least32_t FAST_IO_WINSTDCALL RtlQueryEnvironmentVariable_U(void *, ::fast_io::win32::nt::unicode_string *, ::fast_io::win32::nt::unicode_string *) noexcept FAST_IO_WINSTDCALL_RENAME(RtlQueryEnvironmentVariable_U, 12);
 FAST_IO_DLLIMPORT ::std::uint_least32_t FAST_IO_WINSTDCALL NtSetInformationVirtualMemory(void *, ::fast_io::win32::nt::virtual_memory_information_class, ::std::size_t, ::fast_io::win32::nt::memory_range_entry *, void *, ::std::uint_least32_t) noexcept FAST_IO_WINSTDCALL_RENAME(NtSetInformationVirtualMemory, 24);
-FAST_IO_DLLIMPORT ::std::uint_least32_t FAST_IO_WINSTDCALL ZwSetInformationVirtualMemory(void *, ::fast_io::win32::nt::virtual_memory_information_class, ::std::size_t, ::fast_io::win32::nt::memory_range_entry *, void *, ::std::uint_least32_t) noexcept FAST_IO_WINSTDCALL_RENAME(NtSetInformationVirtualMemory, 24);
+FAST_IO_DLLIMPORT ::std::uint_least32_t FAST_IO_WINSTDCALL ZwSetInformationVirtualMemory(void *, ::fast_io::win32::nt::virtual_memory_information_class, ::std::size_t, ::fast_io::win32::nt::memory_range_entry *, void *, ::std::uint_least32_t) noexcept FAST_IO_WINSTDCALL_RENAME(ZwSetInformationVirtualMemory, 24);
 
 } // namespace fast_io::win32::nt
