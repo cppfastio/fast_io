b3fe830bfa4e0108f913437ca215a2d8aab6a21a
MacroModel <MacroModel_Traject@outlook.com>
2025-06-28 06:15:55 +0800

    fic

--- include/fast_io_hosted/threads/mutex/single.h
+++ include/fast_io_hosted/threads/mutex/single.h
@@ -7,10 +7,14 @@ struct single_thread_noop_mutex
 {
 	inline constexpr void lock() noexcept
 	{}
+	
 	inline constexpr void unlock() noexcept
 	{}
+
 	inline constexpr bool try_lock() noexcept
-	{}
+	{
+		return true;
+	}
 };
 
 } // namespace fast_io
