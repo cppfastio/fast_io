7489f40a6b12809928f871daa8ac00e501a3c415
arendelle <2381642961@qq.com>
2025-07-13 20:03:54 +0800

    Suggesting for is_copy_constructible_v in copy constructor
    ::std::copyable is too strict for copy constructor because it
    requires both is_copy_constructible_v and
    ::std::is_copy_assignable_v. A class has copy constructor but
    disables copy assign operator may sounds weird but it DO troubles
    me. Therefore, if you agree with me, I'll handle other similar
    requires.

--- include/fast_io_dsal/impl/vector.h
+++ include/fast_io_dsal/impl/vector.h
@@ -397,7 +397,7 @@ public:
 	}
 
 	inline constexpr vector(vector const &vec)
-		requires(::std::copyable<value_type>)
+		requires(::std::is_copy_constructible_v<value_type>)
 	{
 		std::size_t const vecsize{static_cast<std::size_t>(vec.imp.curr_ptr - vec.imp.begin_ptr)};
 		if (vecsize == 0)
