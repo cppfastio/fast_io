ed64276ff3bc4d89965317dfb5e5beb306210f05
puji4810 <2430438586@qq.com>
2025-08-18 18:57:55 +0800

    Fix the missing semicolon

--- include/fast_io_core_impl/allocation/custom.h
+++ include/fast_io_core_impl/allocation/custom.h
@@ -6,7 +6,7 @@ namespace fast_io
 class custom_global_allocator;
 
 #if defined(FAST_IO_DISABLE_CUSTOM_THREAD_LOCAL_ALLOCATOR)
-using custom_thread_local_allocator = custom_global_allocator
+using custom_thread_local_allocator = custom_global_allocator;
 #else
 class custom_thread_local_allocator;
 #endif
