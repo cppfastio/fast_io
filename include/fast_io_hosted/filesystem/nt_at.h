c0243f290d5cf98ae256717454b3f101085f5c3a
MacroModel <MacroModel_Traject@outlook.com>
2025-10-28 11:33:03 +0800

    Implement readlinkat functionality for NT filesystem
    - Added a new implementation of `nt_readlinkat_impl` to handle symbolic link resolution in the NT filesystem.
    - Enhanced error handling for symbolic links, including checks for reparse tags.
    - Refactored function signatures for consistency and clarity in the handling of directory handles and paths.
    - Improved overall structure and readability of the code in `nt_at.h`.

--- include/fast_io_hosted/filesystem/nt_at.h
+++ include/fast_io_hosted/filesystem/nt_at.h
@@ -435,8 +435,8 @@ struct nt_create_nothrow_callback
 
 template <bool zw>
 inline void nt_symlinkat_impl([[maybe_unused]] char16_t const *oldpath_c_str, [[maybe_unused]] ::std::size_t oldpath_size,
-							  [[maybe_unused]] void *newdirhd, [[maybe_unused]] char16_t const *newpath_c_str, 
-                              [[maybe_unused]] ::std::size_t newpath_size, [[maybe_unused]] bool kernel)
+							  [[maybe_unused]] void *newdirhd, [[maybe_unused]] char16_t const *newpath_c_str,
+							  [[maybe_unused]] ::std::size_t newpath_size, [[maybe_unused]] bool kernel)
 {
 #if !defined(_WIN32_WINNT) || _WIN32_WINNT > 0x0600
 
@@ -767,9 +767,88 @@ inline void nt_linkat_impl(void *olddirhd, char16_t const *oldpath_c_str, ::std:
 }
 
 template <bool zw, ::std::integral char_type>
-inline ::fast_io::details::basic_ct_string<char_type> nt_readlinkat_impl(void *olddirhd, char16_t const* path_c_str, ::std::size_t path_size, bool kernel)
+inline ::fast_io::details::basic_ct_string<char_type> nt_readlinkat_impl(void *dirhd, char16_t const *path_c_str, ::std::size_t path_size, bool kernel)
 {
+#if !defined(_WIN32_WINNT) || _WIN32_WINNT > 0x0600
+	constexpr ::fast_io::win32::nt::details::nt_open_mode md{
+		.DesiredAccess = 0x00100000 | 0x0080, // SYNCHRONIZE | FILE_READ_ATTRIBUTES
+		.FileAttributes = 0x80,               // FILE_ATTRIBUTE_NORMAL
+		.ShareAccess = 0x00000007,            // FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE
+		.CreateDisposition = 0x00000001,      // OPEN_EXISTING => FILE_OPEN
+		.CreateOptions = 0x00200000           // FILE_FLAG_OPEN_REPARSE_POINT => FILE_OPEN_REPARSE_POINT (0x00200000)
+	};
+
+	::fast_io::basic_nt_family_file<(zw ? nt_family::zw : nt_family::nt), char> file{
+		nt_call_determine_kernel_callback(dirhd, path_c_str, path_size, kernel, nt_create_callback<zw>{md})};
+
+	constexpr ::std::size_t buffer_size{16u * 1024u};
+	alignas(::fast_io::win32::nt::reparse_data_buffer)::std::byte buffer[buffer_size]; // no init
+
+	::fast_io::win32::nt::io_status_block isb;
+
+	auto status{::fast_io::win32::nt::nt_fs_control_file<zw>(file.native_handle(),
+															 nullptr,
+															 nullptr,
+															 nullptr,
+															 __builtin_addressof(isb),
+															 0x900A8,
+															 nullptr,
+															 0u,
+															 buffer,
+															 buffer_size)};
+
+
+	if (status)
+	{
+		throw_nt_error(status);
+	}
+
+	// ::std::size_t const bytesReturned{isb.Information};
+
+	using reparse_data_buffer_const_may_alias_ptr
+#if __has_cpp_attribute(__gnu__::__may_alias__)
+		[[__gnu__::__may_alias__]]
+#endif
+		= ::fast_io::win32::nt::reparse_data_buffer const *;
+
+	auto const reparse_data{reinterpret_cast<reparse_data_buffer_const_may_alias_ptr>(buffer)};
+
+	if (reparse_data->ReparseTag == 0xA000000C /*IO_REPARSE_TAG_SYMLINK*/)
+	{
+		char16_t const *target{reparse_data->u.SymbolicLinkReparseBuffer.PathBuffer +
+							   (reparse_data->u.SymbolicLinkReparseBuffer.SubstituteNameOffset / sizeof(char16_t))};
+		::std::size_t len{static_cast<::std::size_t>(reparse_data->u.SymbolicLinkReparseBuffer.SubstituteNameLength / sizeof(char16_t))};
+		return ::fast_io::details::concat_ct<char_type>(::fast_io::mnp::code_cvt(::fast_io::mnp::strvw(target, target + len)));
+	}
+#if 0
+    else if (reparse_data->ReparseTag == 0xA0000003 /*IO_REPARSE_TAG_MOUNT_POINT*/)
+    {
+        // When simulating the behavior of Linux's readlink() function, you typically only need to handle IO_REPARSE_TAG_SYMLINK.
+    }
+#endif
+	else
+	{
+		throw_nt_error(0xC0000002);
+	}
+
+	return {};
+
+#else
+	constexpr nt_open_mode md{
+		.DesiredAccess = 0x00100000 | 0x0080, // SYNCHRONIZE | FILE_READ_ATTRIBUTES
+		.FileAttributes = 0x80,               // FILE_ATTRIBUTE_NORMAL
+		.ShareAccess = 0x00000007,            // FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE
+		.CreateDisposition = 0x00000001,      // OPEN_EXISTING => FILE_OPEN
+		.CreateOptions = 0x00200000           // FILE_FLAG_OPEN_REPARSE_POINT => FILE_OPEN_REPARSE_POINT (0x00200000)
+	};
+
+	::fast_io::basic_nt_family_file<(zw ? nt_family::zw : nt_family::nt), char> file{
+		nt_call_determine_kernel_callback(dirhd, path_c_str, path_size, kernel, nt_create_callback<zw>{md})};
+
 	throw_nt_error(0xC0000002);
+
+	return {};
+#endif
 }
 
 template <bool zw, ::fast_io::details::posix_api_22 dsp, typename... Args>
@@ -844,7 +923,7 @@ inline auto nt_deal_with1x(void *dir_handle, path_type const &path, Args... args
 {
 	return nt_api_common(
 		path, [&](char16_t const *path_c_str, ::std::size_t path_size) {
-			return nt1x_api_dispatcher < family == nt_family::zw, dsp > (dir_handle, path_c_str, path_size, args...);
+			return nt1x_api_dispatcher<family == nt_family::zw, dsp>(dir_handle, path_c_str, path_size, args...);
 		});
 }
 
@@ -857,7 +936,7 @@ inline auto nt_deal_with12(old_path_type const &oldpath, void *newdirfd, new_pat
 		[&](char16_t const *oldpath_c_str, ::std::size_t oldpath_size) {
 			return nt_api_common(
 				newpath, [&](char16_t const *newpath_c_str, ::std::size_t newpath_size) {
-					return nt12_api_dispatcher < family == nt_family::zw, dsp > (oldpath_c_str, oldpath_size, newdirfd, newpath_c_str, newpath_size, args...);
+					return nt12_api_dispatcher<family == nt_family::zw, dsp>(oldpath_c_str, oldpath_size, newdirfd, newpath_c_str, newpath_size, args...);
 				});
 		});
 }
@@ -869,8 +948,8 @@ inline auto nt_deal_with22(void *olddirhd, oldpath_type const &oldpath, void *ne
 						 [&](char16_t const *oldpath_c_str, ::std::size_t oldpath_size) {
 							 return nt_api_common(newpath,
 												  [&](char16_t const *newpath_c_str, ::std::size_t newpath_size) {
-													  return nt22_api_dispatcher < family == nt_family::zw, dsp > (olddirhd, oldpath_c_str, oldpath_size, newdirhd,
-																												   newpath_c_str, newpath_size, args...);
+													  return nt22_api_dispatcher<family == nt_family::zw, dsp>(olddirhd, oldpath_c_str, oldpath_size, newdirhd,
+																											   newpath_c_str, newpath_size, args...);
 												  });
 						 });
 }
