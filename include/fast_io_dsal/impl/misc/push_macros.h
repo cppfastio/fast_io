928ee76852222922a0f7deec5d69e8f3de78e885
MacroModel <33865334+MacroModel@users.noreply.github.com>
2025-11-01 00:41:51 +0800

    Update push_macros.h

--- include/fast_io_dsal/impl/misc/push_macros.h
+++ include/fast_io_dsal/impl/misc/push_macros.h
@@ -14,10 +14,14 @@
 #undef move
 
 #pragma push_macro("new")
+#if __GNUC__ >= 16
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wkeyword-macro"
 #undef new
 #pragma GCC diagnostic pop
+#else
+#undef new
+#endif
 
 #pragma push_macro("refresh")
 #undef refresh
