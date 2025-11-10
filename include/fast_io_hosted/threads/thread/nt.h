c2b58ba31a4b732efcbee9f3ce7a002eef551289
MacroModel <MacroModel_Traject@outlook.com>
2025-11-10 15:01:26 +0800

    Refactor thread implementation in nt.h and impl.h for clarity and consistency
    - Simplified the namespace structure for `fast_io::win32::nt`, improving organization.
    - Updated thread-related functions and constructors to use `inline constexpr` for better performance and clarity.
    - Enhanced error handling in sleep functions to ensure proper validation of input parameters.
    - Streamlined the usage of `nt_thread` and `this_thread` to improve readability and maintainability across the codebase.

--- include/fast_io_hosted/threads/thread/nt.h
+++ include/fast_io_hosted/threads/thread/nt.h
@@ -9,45 +9,50 @@
 #include "../../../fast_io_dsal/tuple.h"
 #include "../../../fast_io_core_impl/allocation/common.h"
 
-namespace fast_io::win32::nt
+namespace fast_io
+{
+namespace win32::nt
 {
 
 namespace details
 {
 
+template <typename Tuple>
 class nt_thread_start_routine_tuple_allocate_guard
 {
 public:
 	void *ptr_{nullptr};
 
-	constexpr nt_thread_start_routine_tuple_allocate_guard() = default;
+	inline constexpr nt_thread_start_routine_tuple_allocate_guard() = default;
 
-	constexpr nt_thread_start_routine_tuple_allocate_guard(void *ptr) : ptr_{ptr}
+	inline constexpr nt_thread_start_routine_tuple_allocate_guard(void *ptr)
+		: ptr_{ptr}
 	{}
 
-	constexpr nt_thread_start_routine_tuple_allocate_guard(nt_thread_start_routine_tuple_allocate_guard const &) noexcept = delete;
-	constexpr nt_thread_start_routine_tuple_allocate_guard(nt_thread_start_routine_tuple_allocate_guard &&other) noexcept = default;
+	inline constexpr nt_thread_start_routine_tuple_allocate_guard(nt_thread_start_routine_tuple_allocate_guard const &) noexcept = delete;
+	inline constexpr nt_thread_start_routine_tuple_allocate_guard(nt_thread_start_routine_tuple_allocate_guard &&other) noexcept = default;
 
-	constexpr ~nt_thread_start_routine_tuple_allocate_guard()
+	inline constexpr ~nt_thread_start_routine_tuple_allocate_guard()
 	{
 		if (ptr_ != nullptr)
 		{
+			::std::ranges::destroy_at(reinterpret_cast<Tuple *>(this->ptr_));
 			::fast_io::generic_allocator_adapter<::fast_io::nt_rtlallocateheap_allocator>::deallocate(this->ptr_);
 		}
 	}
 };
 
 template <typename Tuple, ::std::size_t... Is>
-constexpr ::std::uint_least32_t FAST_IO_WINSTDCALL thread_start_routine(void *args) noexcept(noexcept(
+inline constexpr ::std::uint_least32_t FAST_IO_WINSTDCALL thread_start_routine(void *args) noexcept(noexcept(
 	::std::invoke(::fast_io::get<Is>(*reinterpret_cast<Tuple *>(args))...)))
 {
-	[[maybe_unused]] ::fast_io::win32::nt::details::nt_thread_start_routine_tuple_allocate_guard _(args);
+	[[maybe_unused]] ::fast_io::win32::nt::details::nt_thread_start_routine_tuple_allocate_guard<Tuple> _(args);
 	::std::invoke(::fast_io::get<Is>(*reinterpret_cast<Tuple *>(args))...);
 	return 0;
 }
 
 template <typename Tuple, ::std::size_t... Is>
-constexpr auto get_thread_start_routine(::std::index_sequence<Is...>) noexcept
+inline constexpr auto get_thread_start_routine(::std::index_sequence<Is...>) noexcept
 {
 	return ::fast_io::win32::nt::details::thread_start_routine<Tuple, Is...>;
 }
@@ -66,13 +71,13 @@ private:
 	native_handle_type handle_{nullptr};
 
 public:
-	constexpr nt_thread() noexcept = default;
+	inline constexpr nt_thread() noexcept = default;
 
 	template <typename Func, typename... Args>
 		requires(::std::invocable<Func, Args...>)
-	constexpr nt_thread(Func &&func, Args &&...args)
+	inline constexpr nt_thread(Func &&func, Args &&...args)
 	{
-		using start_routine_tuple_type = ::fast_io::tuple<decltype(func), decltype(args)...>;
+		using start_routine_tuple_type = ::fast_io::tuple<::std::decay_t<Func>, ::std::decay_t<Args>...>;
 #if __has_cpp_attribute(indeterminate)
 		::fast_io::win32::nt::client_id cid [[indeterminate]];
 #else
@@ -107,11 +112,11 @@ public:
 		this->id_ = cid.UniqueThread;
 	}
 
-	constexpr nt_thread(nt_thread const &) noexcept = delete;
+	inline constexpr nt_thread(nt_thread const &) noexcept = delete;
 
-	constexpr nt_thread(nt_thread &&other) noexcept = default;
+	inline constexpr nt_thread(nt_thread &&other) noexcept = default;
 
-	constexpr ~nt_thread() noexcept
+	inline constexpr ~nt_thread() noexcept
 	{
 		if (handle_ != nullptr)
 		{
@@ -127,9 +132,9 @@ public:
 		}
 	}
 
-	constexpr nt_thread &operator=(nt_thread const &) noexcept = delete;
+	inline constexpr nt_thread &operator=(nt_thread const &) noexcept = delete;
 
-	constexpr nt_thread &operator=(nt_thread &&other) noexcept
+	inline constexpr nt_thread &operator=(nt_thread &&other) noexcept
 	{
 		if (this == __builtin_addressof(other)) [[unlikely]]
 		{
@@ -138,18 +143,18 @@ public:
 		this->swap(other);
 		return *this;
 	}
-	constexpr bool joinable() const noexcept
+	inline constexpr bool joinable() const noexcept
 	{
 		return this->id_ != nullptr;
 	}
 
-	constexpr void join()
+	inline constexpr void join()
 	{
 		if (!this->joinable()) [[unlikely]]
 		{
 			::fast_io::fast_terminate();
 		}
-		auto status{::fast_io::win32::nt::nt_wait_for_single_object<zw>(this->handle_, /* INFINITE = */ int(0xffffffff), nullptr)};
+		auto status{::fast_io::win32::nt::nt_wait_for_single_object<zw>(this->handle_, false, nullptr)};
 		if (status) [[unlikely]]
 		{
 			::fast_io::throw_nt_error(status);
@@ -157,7 +162,7 @@ public:
 		this->id_ = nullptr;
 	}
 
-	constexpr void detach()
+	inline constexpr void detach()
 	{
 		if (!this->joinable()) [[unlikely]]
 		{
@@ -172,20 +177,20 @@ public:
 		this->id_ = nullptr;
 	}
 
-	constexpr void swap(nt_thread &other) noexcept
+	inline constexpr void swap(nt_thread &other) noexcept
 	{
 		::std::ranges::swap(handle_, other.handle_);
 		::std::ranges::swap(id_, other.id_);
 	}
 
 	[[nodiscard]]
-	constexpr auto get_id() const noexcept
+	inline constexpr auto get_id() const noexcept
 	{
 		return this->id_;
 	}
 
 	[[nodiscard]]
-	constexpr auto native_handle() const noexcept
+	inline constexpr auto native_handle() const noexcept
 	{
 		return this->handle_;
 	}
@@ -194,13 +199,22 @@ public:
 	 * @brief Get the win32 id of the thread.
 	 * @note same as win32 GetCurrentThreadId
 	 */
-	constexpr ::std::uint_least32_t get_win32_id() const noexcept
+	inline constexpr ::std::uint_least32_t get_win32_id() const noexcept
 	{
-		auto peb = ::fast_io::win32::nt::nt_current_teb();
-		return static_cast<::std::uint_least32_t>(reinterpret_cast<::std::size_t>(peb->ClientId.UniqueThread));
+		::fast_io::win32::nt::thread_basic_information tbi; // no init
+
+		auto status{::fast_io::win32::nt::nt_query_information_thread<zw>(
+			this->handle_, ::fast_io::win32::nt::thread_information_class::ThreadBasicInformation, __builtin_addressof(tbi), sizeof(tbi), nullptr)};
+
+		if (status) [[unlikely]]
+		{
+			::fast_io::throw_nt_error(status);
+		}
+
+		return reinterpret_cast<::std::uint_least32_t>(tbi.ClientId.UniqueThread);
 	}
 
-	static constexpr ::std::uint_least32_t hardware_concurrency()
+	inline static constexpr ::std::uint_least32_t hardware_concurrency()
 	{
 		::fast_io::win32::nt::system_basic_information sb{};
 		auto status{::fast_io::win32::nt::nt_query_system_information<zw>(::fast_io::win32::nt::system_information_class::SystemBasicInformation,
@@ -217,7 +231,7 @@ namespace this_thread
 {
 
 template <bool zw = false>
-constexpr auto get_id() -> ::fast_io::win32::nt::nt_thread<zw>::id
+inline constexpr ::fast_io::win32::nt::nt_thread<zw>::id get_id()
 {
 	::fast_io::win32::nt::thread_basic_information tbi;
 	::std::uint_least32_t status{::fast_io::win32::nt::nt_query_information_thread<zw>(
@@ -233,10 +247,48 @@ constexpr auto get_id() -> ::fast_io::win32::nt::nt_thread<zw>::id
 	return tbi.ClientId.UniqueThread;
 }
 
-template <bool zw = false, typename Rep, typename Period>
-constexpr void sleep_for(::std::chrono::duration<Rep, Period> const &sleep_duration)
+inline ::std::uint_least32_t get_win32_id() noexcept
 {
-	auto val = -static_cast<::std::int_least64_t>(::std::chrono::duration_cast<::std::chrono::microseconds>(sleep_duration).count() * 10);
+	auto teb{::fast_io::win32::nt::nt_current_teb()};
+	return static_cast<::std::uint_least32_t>(reinterpret_cast<::std::size_t>(teb->ClientId.UniqueThread));
+}
+
+template <bool zw = false, typename Rep, typename Period>
+inline constexpr void sleep_for(::std::chrono::duration<Rep, Period> const &sleep_duration)
+{
+	auto const count{
+		::std::chrono::duration_cast<::std::chrono::microseconds>(sleep_duration).count() * 10u +
+		::std::chrono::duration_cast<::std::chrono::nanoseconds>(sleep_duration).count() / 100u % 10u};
+
+	if (count > static_cast<::std::uint_least64_t>(::std::numeric_limits<::std::int_least64_t>::max()))
+	{
+		::fast_io::throw_nt_error(0xC000000D);
+	}
+
+	auto val{-static_cast<::std::int_least64_t>(count)};
+	::std::uint_least32_t status{::fast_io::win32::nt::nt_delay_execution<zw>(false, __builtin_addressof(val))};
+	if (status) [[unlikely]]
+	{
+		::fast_io::throw_nt_error(status);
+	}
+}
+
+template <bool zw = false, ::std::int_least64_t off_to_epoch>
+inline constexpr void sleep_for(::fast_io::basic_timestamp<off_to_epoch> const &sleep_duration)
+{
+	if (sleep_duration.seconds < 0) [[unlikely]]
+	{
+		::fast_io::throw_nt_error(0xC000000D);
+	}
+
+	auto const win_100ns_seconds{static_cast<::std::uint_least64_t>(static_cast<::std::uint_least64_t>(sleep_duration.seconds) * 10'000'000u + sleep_duration.subseconds / 100u)};
+
+	if (win_100ns_seconds > static_cast<::std::uint_least64_t>(::std::numeric_limits<::std::int_least64_t>::max())) [[unlikely]]
+	{
+		::fast_io::throw_nt_error(0xC000000D);
+	}
+
+	auto val = -static_cast<::std::int_least64_t>(win_100ns_seconds);
 	::std::uint_least32_t status{::fast_io::win32::nt::nt_delay_execution<zw>(false, __builtin_addressof(val))};
 	if (status) [[unlikely]]
 	{
@@ -245,13 +297,56 @@ constexpr void sleep_for(::std::chrono::duration<Rep, Period> const &sleep_durat
 }
 
 template <bool zw = false, typename Clock, typename Duration>
-constexpr void sleep_until(::std::chrono::time_point<Clock, Duration> const &expect_time)
+inline constexpr void sleep_until(::std::chrono::time_point<Clock, Duration> const &expect_time)
 {
 	auto const unix_ts = ::std::chrono::duration_cast<std::chrono::seconds>(
-					   expect_time.time_since_epoch())
-					   .count();
-	auto nt_ts = (unix_ts + 11644473600) * 10000000;
+							 expect_time.time_since_epoch())
+							 .count();
+	auto const unix_subsec_ts = ::std::chrono::duration_cast<std::chrono::nanoseconds>(
+									expect_time.time_since_epoch())
+									.count() /
+								100u % 10000000u;
+
+	auto const unix_to_nt_secs{static_cast<::std::int_least64_t>(unix_ts) + 11644473600};
+	if (unix_to_nt_secs < 0) [[unlikely]]
+	{
+		::fast_io::throw_nt_error(0xC000000D);
+	}
+
+	auto const count{static_cast<::std::uint_least64_t>(unix_to_nt_secs) * 10000000u + static_cast<::std::uint_least64_t>(unix_subsec_ts)};
+
+	if (count > static_cast<::std::uint_least64_t>(::std::numeric_limits<::std::int_least64_t>::max()))
+	{
+		::fast_io::throw_nt_error(0xC000000D);
+	}
+
+	auto nt_ts{static_cast<::std::int_least64_t>(count)};
 	::std::uint_least32_t status{::fast_io::win32::nt::nt_delay_execution<zw>(false, __builtin_addressof(nt_ts))};
+	if (status) [[unlikely]]
+	{
+		::fast_io::throw_nt_error(status);
+	}
+}
+
+template <bool zw = false, ::std::int_least64_t off_to_epoch>
+inline constexpr void sleep_until(::fast_io::basic_timestamp<off_to_epoch> const &expect_time)
+{
+	if (expect_time.seconds < 0) [[unlikely]]
+	{
+		::fast_io::throw_nt_error(0xC000000D);
+	}
+
+	auto const win32_ts{static_cast<::fast_io::win32_timestamp>(expect_time)};
+
+	auto const win_100ns_seconds = static_cast<::std::uint_least64_t>(win32_ts.seconds) * 10'000'000u + win32_ts.subseconds / 100u;
+
+	if (win_100ns_seconds > static_cast<::std::uint_least64_t>(::std::numeric_limits<::std::int_least64_t>::max())) [[unlikely]]
+	{
+		::fast_io::throw_nt_error(0xC000000D);
+	}
+
+	auto val = static_cast<::std::int_least64_t>(win_100ns_seconds);
+	::std::uint_least32_t status{::fast_io::win32::nt::nt_delay_execution<zw>(false, __builtin_addressof(val))};
 	if (status) [[unlikely]]
 	{
 		::fast_io::throw_nt_error(status);
@@ -260,4 +355,20 @@ constexpr void sleep_until(::std::chrono::time_point<Clock, Duration> const &exp
 
 } // namespace this_thread
 
-} // namespace fast_io::win32::nt
+} // namespace win32::nt
+
+using nt_thread = win32::nt::nt_thread<false>;
+using zw_thread = win32::nt::nt_thread<true>;
+
+#if defined(_WIN32) && !defined(_WIN32_WINDOWS)
+using native_thread = nt_thread;
+
+namespace this_thread
+{
+using ::fast_io::win32::nt::this_thread::get_id;
+using ::fast_io::win32::nt::this_thread::sleep_for;
+using ::fast_io::win32::nt::this_thread::sleep_until;
+} // namespace this_thread
+#endif
+
+} // namespace fast_io
