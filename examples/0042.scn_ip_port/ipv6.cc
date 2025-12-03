baed16caea4bcede34f32fb6987a54b16aaa1fc9
MacroModel <MacroModel_Traject@outlook.com>
2025-12-03 15:02:28 +0800

    Mark scn_ip_port example as interactive in test configuration
    - Added `interactive = true` flag for `0042.scn_ip_port` test case to indicate it requires user input

---
+++ examples/0042.scn_ip_port/ipv6.cc
@@ -0,0 +1,9 @@
+﻿#include <fast_io.h>
+
+int main()
+{
+    ::fast_io::ipv6 v6;
+	//auto i = ::fast_io::mnp::ip_scan_generic<::fast_io::mnp::ip_scan_default_flags>(v4);
+	::fast_io::scan(::fast_io::mnp::ip_scan_generic<::fast_io::mnp::ip_scan_default_flags>(v6));
+	::fast_io::perrln(v6);
+}
