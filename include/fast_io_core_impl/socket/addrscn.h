79bd1c8250d318d4a9b71406dacd521c533d4f11
MacroModel <33865334+MacroModel@users.noreply.github.com>
2025-12-03 18:32:02 +0800

    Update addrscn.h

--- include/fast_io_core_impl/socket/addrscn.h
+++ include/fast_io_core_impl/socket/addrscn.h
@@ -23,8 +23,14 @@ enum class scan_integral_context_phase : ::std::uint_least8_t;
 template <typename phase_t, ::std::integral char_type>
 struct ip_scan_state_t
 {
-	/// @todo forgot?
-	// TODO: to find out why +1 is needed here
+	// to find out why +1 is needed here:
+	
+	// Reserve one extra character beyond the maximum decimal length of
+	// uint_least16_t so that the streaming integer scanner
+	// (scan_context_define_parse_impl / sc_int_ctx_digit_phase) can
+	// accept the longest valid value without treating it as an
+	// overflow/partial state at the length boundary.
+
 	static inline constexpr auto max_size{
 		::fast_io::details::print_integer_reserved_size_cache<10, false, false, ::std::uint_least16_t> + 1};
 	::fast_io::freestanding::array<char_type, max_size> buffer;
